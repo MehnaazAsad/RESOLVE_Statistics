db89fe65ff2c8b3dbd78f25ce42c9fffdfaee54b
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2018-06-18 15:20:44 -0500

    Fixed animation of error bars

--- src/data/main/SMF.py
+++ src/data/main/SMF.py
@@ -36,7 +36,6 @@ rc('text', usetex=True)
 plt.rcParams['animation.convert_path'] = '/fs1/masad/anaconda3/envs/resolve_statistics/bin/magick'
 #'{0}/magick'.format(os.path.dirname(which('python')))
 
-#path_to_interim = '/Users/asadm2/Documents/Grad_School/Research/Repositories/resolve_statistics/data/interim/'
 RESOLVE = pd.read_csv(path_to_interim + 'RESOLVE_formatted.txt',delimiter='\t')
 
 M_HI = []
@@ -63,19 +62,23 @@ V_resolve = 5.2e4  #Survey volume in Mpc3
 #Unnormalized histogram and bin edges
 Phi_resolve,edg_resolve = np.histogram(logM_resolve,bins=nbins_resolve)  
 dM_resolve = edg_resolve[1] - edg_resolve[0]  #Bin width
-Max_resolve = edg_resolve[0:-1] + dM_resolve/2.  #Mass axis
+Max_resolve = 0.5*(edg_resolve[1:]+edg_resolve[:-1])  #Mass axis i.e. bin centers
 #Normalized to volume and bin width
+menStd     = np.sqrt(Phi_resolve)/(V_resolve*dM_resolve)
 Phi_resolve = Phi_resolve/(V_resolve*dM_resolve)  
 
 fig1 = plt.figure(figsize=(10,8))
 plt.yscale('log')
-#plt.axvline(x=8.9, ls = '--', c = 'r')
 plt.xlim(8.9,11.5)
+plt.errorbar(Max_resolve,Phi_resolve,yerr=menStd,fmt="rs--",    
+    linewidth=2,   # width of plot line
+    elinewidth=0.5,# width of error bar line
+    ecolor='k',    # color of error bar
+    capsize=5,     # cap length for error bar
+    capthick=0.5 )
 plt.xlabel(r'$\log(M_\star\,/\,M_\odot)$')
 plt.ylabel(r'$\Phi\,/\,\mathrm{dex}^{-1}\,\mathrm{Mpc}^{-3}$')
-plt.plot(Max_resolve,Phi_resolve)
 plt.title('Stellar mass function')
-
 
 counter = 0
 nbins = 10
@@ -86,11 +89,6 @@ Mstellar_characteristic = np.arange(9.5,11.0,0.1) #11.0 not included
 Mlow_slope = np.arange(0.35,0.50,0.01)[:-1] #0.5 included by default
 Mhigh_slope = np.arange(0.50,0.65,0.01)[:-1]
 Mstellar_scatter = np.arange(0.02,0.095,0.005)
-#Mhalo_characteristic = Mhalo_characteristic[:2]
-#Mstellar_characteristic = Mstellar_characteristic[:2]
-#Mlow_slope = Mlow_slope[:2]
-#Mhigh_slope = Mhigh_slope[:2]
-#Mstellar_scatter = Mstellar_scatter[:2]
 
 def gals(Mhalo_value,Mstellar_value,Mlow_slope,Mhigh_slope,Mstellar_scatter):   
     ###Models
@@ -111,12 +109,7 @@ def gals(Mhalo_value,Mstellar_value,Mlow_slope,Mhigh_slope,Mstellar_scatter):
     halocat3 = CachedHaloCatalog(fname=halo_catalog)
     halocat4 = CachedHaloCatalog(fname=halo_catalog)
     halocat5 = CachedHaloCatalog(fname=halo_catalog)
-#    halocat1 = FakeSim()
-#    halocat2 = FakeSim()
-#    halocat3 = FakeSim()
-#    halocat4 = FakeSim()
-#    halocat5 = FakeSim()
-    
+
     ###Parameter values
     model1.param_dict['smhm_m1_0'] = Mhalo_value
     model2.param_dict['smhm_m0_0'] = Mstellar_value
@@ -147,12 +140,24 @@ def gals(Mhalo_value,Mstellar_value,Mlow_slope,Mhigh_slope,Mstellar_scatter):
     return Mhalo_gals,Mstellar_gals,Mlowslope_gals,Mhighslope_gals,Mstellarscatter_gals
     
 def init():
-    line1, = ax1.plot([],[],lw=2,color='k')   
-    line2, = ax2.plot([],[],lw=2,color='k')
-    line3, = ax3.plot([],[],lw=2,color='k')
-    line4, = ax4.plot([],[],lw=2,color='k')
-    line5, = ax5.plot([],[],lw=2,color='k')
+    
+    line1 = ax1.errorbar([],[],yerr=[],fmt="bs--",\
+                            linewidth=2,elinewidth=0.5,ecolor='k',capsize=2,\
+                            capthick=0.5)
+    line2 = ax2.errorbar([],[],yerr=[],fmt="bs--",\
+                            linewidth=2,elinewidth=0.5,ecolor='k',capsize=2,\
+                            capthick=0.5)     
+    line3 = ax3.errorbar([],[],yerr=[],fmt="bs--",\
+                            linewidth=2,elinewidth=0.5,ecolor='k',capsize=2,\
+                            capthick=0.5)     
+    line4 = ax4.errorbar([],[],yerr=[],fmt="bs--",\
+                            linewidth=2,elinewidth=0.5,ecolor='k',capsize=2,\
+                            capthick=0.5)     
+    line5 = ax5.errorbar([],[],yerr=[],fmt="bs--",\
+                        linewidth=2,elinewidth=0.5,ecolor='k',capsize=2,\
+                        capthick=0.5) 
     line = [line1,line2,line3,line4,line5]
+    
     return line
 
 def make_animation(i,j,k,l,m):
@@ -180,17 +185,52 @@ def make_animation(i,j,k,l,m):
     ax4_dM = ax4_edg[1] - ax4_edg[0]
     ax5_dM = ax5_edg[1] - ax5_edg[0]
     
-    ax1_Max = ax1_edg[0:-1] + ax1_dM/2.
-    ax2_Max = ax2_edg[0:-1] + ax2_dM/2.
-    ax3_Max = ax3_edg[0:-1] + ax3_dM/2.
-    ax4_Max = ax4_edg[0:-1] + ax4_dM/2.
-    ax5_Max = ax5_edg[0:-1] + ax5_dM/2.
+    ax1_Max = 0.5*(ax1_edg[1:]+ax1_edg[:-1])
+    ax2_Max = 0.5*(ax2_edg[1:]+ax2_edg[:-1])
+    ax3_Max = 0.5*(ax3_edg[1:]+ax3_edg[:-1])
+    ax4_Max = 0.5*(ax4_edg[1:]+ax4_edg[:-1])
+    ax5_Max = 0.5*(ax5_edg[1:]+ax5_edg[:-1])
     
+    ax1_menStd = np.sqrt(Phi1)/(Volume_FK*ax1_dM)
+    ax2_menStd = np.sqrt(Phi2)/(Volume_FK*ax2_dM)
+    ax3_menStd = np.sqrt(Phi3)/(Volume_FK*ax3_dM)
+    ax4_menStd = np.sqrt(Phi4)/(Volume_FK*ax4_dM)
+    ax5_menStd = np.sqrt(Phi5)/(Volume_FK*ax5_dM)
+   
     Phi1 = Phi1/(float(Volume_FK)*ax1_dM)
     Phi2 = Phi2/(float(Volume_FK)*ax2_dM)
     Phi3 = Phi3/(float(Volume_FK)*ax3_dM)
     Phi4 = Phi4/(float(Volume_FK)*ax4_dM)
     Phi5 = Phi5/(float(Volume_FK)*ax5_dM)
+    
+    for ax in [ax1,ax2,ax3,ax4,ax5]:
+        ax.clear()
+        SMF_RESOLVE = ax.errorbar(Max_resolve,Phi_resolve,yerr=menStd,\
+                                  fmt="rs--",linewidth=2,elinewidth=0.5,\
+                                  ecolor='k',capsize=2,capthick=1.5,\
+                                  markersize='3')
+        ax.set_xlim(8.9,)
+        ax.minorticks_on()
+        ax.set_yscale('log')
+        ax.set_xlabel(r'$\log(M_\star\,/\,M_\odot)$')
+        ax.set_ylabel(r'$\Phi\,/\,\mathrm{dex}^{-1}\,\mathrm{Mpc}^{-3}$')
+
+    line1 = ax1.errorbar(ax1_Max,Phi1,yerr=ax1_menStd,fmt="bs--",\
+                            linewidth=2,elinewidth=0.5,ecolor='k',capsize=2,\
+                            capthick=1.5,markersize='3') 
+    line2 = ax2.errorbar(ax2_Max,Phi2,yerr=ax2_menStd,fmt="bs--",\
+                            linewidth=2,elinewidth=0.5,ecolor='k',capsize=2,\
+                            capthick=1.5,markersize='3') 
+    line3 = ax3.errorbar(ax3_Max,Phi3,yerr=ax3_menStd,fmt="bs--",\
+                            linewidth=2,elinewidth=0.5,ecolor='k',capsize=2,\
+                            capthick=1.5,markersize='3') 
+    line4 = ax4.errorbar(ax4_Max,Phi4,yerr=ax4_menStd,fmt="bs--",\
+                            linewidth=2,elinewidth=0.5,ecolor='k',capsize=2,\
+                            capthick=1.5,markersize='3') 
+    line5 = ax5.errorbar(ax5_Max,Phi5,yerr=ax5_menStd,fmt="bs--",\
+                            linewidth=2,elinewidth=0.5,ecolor='k',capsize=2,\
+                            capthick=1.5,markersize='3') 
+
     
     ax1.legend([line1,SMF_RESOLVE],[r'$M_{h}=%4.2f$' % Mhalo,'RESOLVE'],\
                loc='lower left',prop={'size': 10})
@@ -205,15 +245,12 @@ def make_animation(i,j,k,l,m):
     
     print('Setting data')
     print('Frame {0}/{1}'.format(counter+1,len(Mhalo_characteristic)))
-    line1.set_data(ax1_Max,Phi1)
-    line2.set_data(ax2_Max,Phi2)
-    line3.set_data(ax3_Max,Phi3)
-    line4.set_data(ax4_Max,Phi4)
-    line5.set_data(ax5_Max,Phi5)
     
     counter+=1
     
-    return [line1,line2,line3,line4,line5]  
+    line = [line1,line2,line3,line4,line5]  
+    
+    return line
 
 #Setting up the figure, the axis, and the plot element we want to animate
 fig = plt.figure(figsize=(10,8))
@@ -223,57 +260,6 @@ ax3 = plt.subplot2grid((2,6), (0,4), colspan=2)
 ax4 = plt.subplot2grid((2,6), (1,1), colspan=2)
 ax5 = plt.subplot2grid((2,6), (1,3), colspan=2)
 
-ax1_catalog,ax2_catalog,ax3_catalog,ax4_catalog,ax5_catalog = \
-gals(Mhalo_characteristic[0],Mstellar_characteristic[0],Mlow_slope[0],\
-     Mhigh_slope[0],Mstellar_scatter[0])
-
-ax1_logM = np.log10(ax1_catalog['stellar_mass'])
-ax1_Phi,ax1_edg = np.histogram(ax1_logM,bins=nbins) 
-ax1_dM = ax1_edg[1] - ax1_edg[0]  
-ax1_Max = ax1_edg[0:-1] + ax1_dM/2.
-ax1_Phi = ax1_Phi/(float(Volume_FK)*ax1_dM)
-line1, = ax1.plot(ax1_Max,ax1_Phi)
-SMF_RESOLVE, = ax1.plot(Max_resolve,Phi_resolve,label='SMF RESOLVE',c='r')
-
-ax2_logM = np.log10(ax2_catalog['stellar_mass'])
-ax2_Phi,ax2_edg = np.histogram(ax2_logM,bins=nbins) 
-ax2_dM = ax2_edg[1] - ax2_edg[0]  
-ax2_Max = ax2_edg[0:-1] + ax2_dM/2.
-ax2_Phi = ax2_Phi/(float(Volume_FK)*ax2_dM)
-line2, = ax2.plot(ax2_Max,ax2_Phi)
-SMF_RESOLVE, = ax2.plot(Max_resolve,Phi_resolve,label='SMF RESOLVE',c='r')
-
-ax3_logM = np.log10(ax3_catalog['stellar_mass'])
-ax3_Phi,ax3_edg = np.histogram(ax3_logM,bins=nbins) 
-ax3_dM = ax3_edg[1] - ax3_edg[0]  
-ax3_Max = ax3_edg[0:-1] + ax3_dM/2.
-ax3_Phi = ax3_Phi/(float(Volume_FK)*ax3_dM)
-line3, = ax3.plot(ax3_Max,ax3_Phi)
-SMF_RESOLVE, = ax3.plot(Max_resolve,Phi_resolve,label='SMF RESOLVE',c='r')
-
-ax4_logM = np.log10(ax4_catalog['stellar_mass'])
-ax4_Phi,ax4_edg = np.histogram(ax4_logM,bins=nbins) 
-ax4_dM = ax4_edg[1] - ax4_edg[0]  
-ax4_Max = ax4_edg[0:-1] + ax4_dM/2.
-ax4_Phi = ax4_Phi/(float(Volume_FK)*ax4_dM)
-line4, = ax4.plot(ax4_Max,ax4_Phi)
-SMF_RESOLVE, = ax4.plot(Max_resolve,Phi_resolve,label='SMF RESOLVE',c='r')
-
-ax5_logM = np.log10(ax5_catalog['stellar_mass'])
-ax5_Phi,ax5_edg = np.histogram(ax5_logM,bins=nbins) 
-ax5_dM = ax5_edg[1] - ax5_edg[0]  
-ax5_Max = ax5_edg[0:-1] + ax5_dM/2.
-ax5_Phi = ax5_Phi/(float(Volume_FK)*ax5_dM)
-line5, = ax5.plot(ax5_Max,ax5_Phi)
-SMF_RESOLVE, = ax5.plot(Max_resolve,Phi_resolve,label='SMF RESOLVE',c='r')
-
-for ax in [ax1,ax2,ax3,ax4,ax5]:
-    ax.set_xlim(8.9,)
-    ax.minorticks_on()
-    ax.set_yscale('log')
-    ax.set_xlabel(r'$\log(M_\star\,/\,M_\odot)$')
-    ax.set_ylabel(r'$\Phi\,/\,\mathrm{dex}^{-1}\,\mathrm{Mpc}^{-3}$')
-
 anim = animation.FuncAnimation(plt.gcf(), make_animation, \
                                Mhalo_characteristic,init_func=init,\
                                fargs=(Mstellar_characteristic,Mlow_slope,\
@@ -281,13 +267,6 @@ anim = animation.FuncAnimation(plt.gcf(), make_animation, \
                                       interval=1000,blit=False,repeat=True)
 plt.tight_layout()
 print('Saving animation')
-#plt.show()
-#Writer = animation.writers['imagemagick']
-#writer = Writer(fps=15, bitrate=1800)
-#anim.save('SMF.html')
-#animation.verbose.set_level('helpful')
 os.chdir(path_to_figures)
-#writer = ImageMagickFileWriter()
 anim.save('SMF_mvir.gif',writer='imagemagick',fps=1)
-#anim.save('SMF_5params.html',fps=1)
 
