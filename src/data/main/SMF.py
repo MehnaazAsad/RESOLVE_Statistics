eece2a874552e5e73819bdb432f0c8df7d72a7b2
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2018-06-21 14:29:49 -0500

    RESOLVE B

--- src/data/main/SMF.py
+++ src/data/main/SMF.py
@@ -5,7 +5,7 @@ Created on Mon May 28 16:22:17 2018
 
 @author: asadm2
 """
-
+#%%
 ### DESCRIPTION
 #This script parametrizes the SMHM relation to produce a SMF which is compared
 #to the SMF from RESOVLE
@@ -36,41 +36,49 @@ rc('text', usetex=True)
 plt.rcParams['animation.convert_path'] = '/fs1/masad/anaconda3/envs/resolve_statistics/bin/magick'
 #'{0}/magick'.format(os.path.dirname(which('python')))
 
+
 RESOLVE = pd.read_csv(path_to_interim + 'RESOLVE_formatted.txt',delimiter='\t')
+RESOLVE_A = RESOLVE[RESOLVE['Name'].str.contains("rs")] #969
+RESOLVE_B = RESOLVE[RESOLVE['Name'].str.contains("rf")] #402
+
 
 M_HI = []
 M_stellar = []
 M_halo_s = [] #group halo mass from stellar mass limited sample
 M_halo_b = [] #group halo mass from baryonic mass limited sample
 H_0 = 70 #km/s/Mpc
-for galaxy in RESOLVE.Name.values:
-    f21 = RESOLVE.F21.loc[RESOLVE.Name == galaxy].values[0]
-    cz = RESOLVE.cz.loc[RESOLVE.Name == galaxy].values[0]
+for galaxy in RESOLVE_B.Name.values:
+    f21 = RESOLVE_B.F21.loc[RESOLVE_B.Name == galaxy].values[0]
+    cz = RESOLVE_B.cz.loc[RESOLVE_B.Name == galaxy].values[0]
     D = cz/H_0 #Mpc
     m_HI = (2.36*10**5)*(D**2)*(f21)
     M_HI.append(np.log10(m_HI))
-    M_stellar.append(RESOLVE['logstellarmass'].loc[RESOLVE.Name == galaxy].\
+    M_stellar.append(RESOLVE_B['logstellarmass'].loc[RESOLVE_B.Name == galaxy].\
                      values[0])
-    M_halo_s.append(RESOLVE['groupmass-s'].loc[RESOLVE.Name == galaxy]\
+    M_halo_s.append(RESOLVE_B['groupmass-s'].loc[RESOLVE_B.Name == galaxy]\
                     .values[0])
-    M_halo_b.append(RESOLVE['groupmass-b'].loc[RESOLVE.Name == galaxy]\
+    M_halo_b.append(RESOLVE_B['groupmass-b'].loc[RESOLVE_B.Name == galaxy]\
                     .values[0])
+
 
 logM_resolve  = M_stellar  #Read stellar masses
 nbins_resolve = 10  #Number of bins to divide data into
-V_resolve = 5.2e4  #Survey volume in Mpc3
+V_resolve = 13700 #RESOLVE-B volume in Mpc3
 #Unnormalized histogram and bin edges
 Phi_resolve,edg_resolve = np.histogram(logM_resolve,bins=nbins_resolve)  
 dM_resolve = edg_resolve[1] - edg_resolve[0]  #Bin width
 Max_resolve = 0.5*(edg_resolve[1:]+edg_resolve[:-1])  #Mass axis i.e. bin centers
 #Normalized to volume and bin width
-menStd     = np.sqrt(Phi_resolve)/(V_resolve*dM_resolve)
+err_poiss = np.sqrt(Phi_resolve)/(V_resolve*dM_resolve)
+err_cvar = 0.58/(V_resolve*dM_resolve)
+err_tot = np.sqrt(err_cvar**2 + err_poiss**2)
+
 Phi_resolve = Phi_resolve/(V_resolve*dM_resolve)  
 
 fig1 = plt.figure(figsize=(10,8))
 plt.yscale('log')
 plt.xlim(8.9,11.5)
-plt.errorbar(Max_resolve,Phi_resolve,yerr=menStd,fmt="rs--",    
+plt.errorbar(Max_resolve,Phi_resolve,yerr=err_tot,fmt="rs--",    
     linewidth=2,   # width of plot line
     elinewidth=0.5,# width of error bar line
     ecolor='k',    # color of error bar
@@ -79,10 +87,56 @@ plt.errorbar(Max_resolve,Phi_resolve,yerr=menStd,fmt="rs--",
 plt.xlabel(r'$\log(M_\star\,/\,M_\odot)$')
 plt.ylabel(r'$\Phi\,/\,\mathrm{dex}^{-1}\,\mathrm{Mpc}^{-3}$')
 plt.title('Stellar mass function')
+###############################################################################
+###COSMIC VARIANCE
+eco = pd.read_csv(path_to_interim + 'ECO_DR1.txt',delimiter='\s+',header=None,usecols=\
+                            [0,1,2,3,4,5,13,15],\
+                            names=['ECOID','RA','DEC','velocity','M_r',\
+                                   'log_stellarmass','groupcz',\
+                                   'log_halogroupmass'])
+    
+dec_slices = np.arange(-1,49.85,2.5)
+minz_resB = np.min(RESOLVE_B.cz/(3*10**5))
+maxz_resB = np.max(RESOLVE_B.cz/(3*10**5))
+mincz_resB = 4500
+maxcz_resB = 7000
+H_0 = 70
+slice_area = 2.5*75 #deg^2
+deg2_in_sphere = 41252.96 #deg^2
 
+num_in_slice = []
+z = []
+for index,value in enumerate(dec_slices):
+    if index == 20:
+        break
+    counter = 0
+    for index2,galaxy in enumerate(eco.ECOID.values):
+        if 134.99 < eco.RA.values[index2] < 209.99:
+            if value < eco.DEC.values[index2] < dec_slices[index+1]:
+                if minz_resB < (eco.velocity.values[index2]/(3*10**5)) < maxz_resB:
+                    z.append(eco.velocity.values[index2]/(3*10**5))
+                    counter += 1
+    num_in_slice.append(counter)
+num_in_slice = np.array(num_in_slice)
+
+#med_z = np.median(z)
+#d = med_z*3*10**5/H_0 #Mpc
+#vol_sphere = (4/3)*np.pi*(d**3) #Mpc^3
+
+inner_vol = (4/3)*np.pi*((mincz_resB/H_0)**3)
+outer_vol = (4/3)*np.pi*((maxcz_resB/H_0)**3)
+vol_sphere = outer_vol-inner_vol #Mpc^3
+
+vol_slice = (slice_area/deg2_in_sphere)*vol_sphere
+gal_dens = num_in_slice/vol_slice
+cosmic_variance = (np.std(gal_dens)*100)
+print('Cosmic variance: {0}%'.format(np.round(cosmic_variance,2)))
+###############################################################################
+
+#%%
 counter = 0
 nbins = 10
-Volume = 5.2e4  #Volume of RESOLVE Mpc^3
+Volume =  V_resolve #Volume of RESOLVE-B Mpc^3
 Volume_FK = 250.**3
 Mhalo_characteristic = np.arange(11.5,13.0,0.1) #13.0 not included
 Mstellar_characteristic = np.arange(9.5,11.0,0.1) #11.0 not included
@@ -124,12 +178,12 @@ def gals(Mhalo_value,Mstellar_value,Mlow_slope,Mhigh_slope,Mstellar_scatter):
     model4.populate_mock(halocat4)
     model5.populate_mock(halocat5)
     
-    #Applying RESOLVE-A stellar mass limit
-    sample_mask1 = model1.mock.galaxy_table['stellar_mass'] >= 10**8.9
-    sample_mask2 = model2.mock.galaxy_table['stellar_mass'] >= 10**8.9
-    sample_mask3 = model3.mock.galaxy_table['stellar_mass'] >= 10**8.9
-    sample_mask4 = model4.mock.galaxy_table['stellar_mass'] >= 10**8.9
-    sample_mask5 = model5.mock.galaxy_table['stellar_mass'] >= 10**8.9
+    #Applying RESOLVE-B stellar mass limit
+    sample_mask1 = model1.mock.galaxy_table['stellar_mass'] >= 10**8.7
+    sample_mask2 = model2.mock.galaxy_table['stellar_mass'] >= 10**8.7
+    sample_mask3 = model3.mock.galaxy_table['stellar_mass'] >= 10**8.7
+    sample_mask4 = model4.mock.galaxy_table['stellar_mass'] >= 10**8.7
+    sample_mask5 = model5.mock.galaxy_table['stellar_mass'] >= 10**8.7
     
     Mhalo_gals            = model1.mock.galaxy_table[sample_mask1]
     Mstellar_gals         = model2.mock.galaxy_table[sample_mask2]
@@ -205,7 +259,7 @@ def make_animation(i,j,k,l,m):
     
     for ax in [ax1,ax2,ax3,ax4,ax5]:
         ax.clear()
-        SMF_RESOLVE = ax.errorbar(Max_resolve,Phi_resolve,yerr=menStd,\
+        SMF_RESOLVEB = ax.errorbar(Max_resolve,Phi_resolve,yerr=err_tot,\
                                   fmt="rs--",linewidth=2,elinewidth=0.5,\
                                   ecolor='k',capsize=2,capthick=1.5,\
                                   markersize='3')
@@ -232,15 +286,15 @@ def make_animation(i,j,k,l,m):
                             capthick=1.5,markersize='3') 
 
     
-    ax1.legend([line1,SMF_RESOLVE],[r'$M_{h}=%4.2f$' % Mhalo,'RESOLVE'],\
+    ax1.legend([line1,SMF_RESOLVEB],[r'$M_{h}=%4.2f$' % Mhalo,'RESOLVE-B'],\
                loc='lower left',prop={'size': 10})
-    ax2.legend([line2,SMF_RESOLVE],[r'$M_{*}=%4.2f$' % Mstellar,'RESOLVE'],\
+    ax2.legend([line2,SMF_RESOLVEB],[r'$M_{*}=%4.2f$' % Mstellar,'RESOLVE-B'],\
                loc='lower left',prop={'size': 10})
-    ax3.legend([line3,SMF_RESOLVE],[r'$\beta=%4.2f$' % Mlowslope,'RESOLVE'],\
+    ax3.legend([line3,SMF_RESOLVEB],[r'$\beta=%4.2f$' % Mlowslope,'RESOLVE-B'],\
                loc='lower left',prop={'size': 10})
-    ax4.legend([line4,SMF_RESOLVE],[r'$\delta=%4.2f$' % Mhighslope,'RESOLVE'],\
+    ax4.legend([line4,SMF_RESOLVEB],[r'$\delta=%4.2f$' % Mhighslope,'RESOLVE-B'],\
                loc='lower left',prop={'size': 10})
-    ax5.legend([line5,SMF_RESOLVE],[r'$\xi=%4.3f$' % Mstellarscatter,'RESOLVE']\
+    ax5.legend([line5,SMF_RESOLVEB],[r'$\xi=%4.3f$' % Mstellarscatter,'RESOLVE-B']\
                ,loc='lower left',prop={'size': 10})
     
     print('Setting data')
@@ -268,5 +322,5 @@ anim = animation.FuncAnimation(plt.gcf(), make_animation, \
 plt.tight_layout()
 print('Saving animation')
 os.chdir(path_to_figures)
-anim.save('SMF_macc.gif',writer='imagemagick',fps=1)
+anim.save('SMF_RB_macc.gif',writer='imagemagick',fps=1)
 
