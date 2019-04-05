8560a82d64002de1bcd0f467adc5a163b2809218
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2019-04-05 12:04:29 -0500

    Animation for ECO with extremem scatter param values

--- src/data/main/SMF.py
+++ src/data/main/SMF.py
@@ -21,6 +21,12 @@ import pandas as pd
 import numpy as np
 import os
 
+def which(pgm):
+    path=os.getenv('PATH')
+    for p in path.split(os.path.pathsep):
+        p=os.path.join(p,pgm)
+        if os.path.exists(p) and os.access(p,os.X_OK):
+            return p
 
 ### Paths
 dict_of_paths = cwpaths.cookiecutter_paths()
@@ -29,7 +35,7 @@ path_to_interim = dict_of_paths['int_dir']
 path_to_figures = dict_of_paths['plot_dir']
 halo_catalog = '/home/asadm2/.astropy/cache/halotools/halo_catalogs/vishnu/'\
 'rockstar/vishnu_rockstar_test.hdf5'
-#halo_catalog = '/Users/asadm2/Desktop/vishnu_rockstar_test.hdf5'
+# halo_catalog = '/Users/asadm2/Desktop/vishnu_rockstar_test.hdf5'
 
 ###Formatting for plots and animation
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']},size=15)
@@ -37,48 +43,77 @@ rc('text', usetex=True)
 plt.rcParams['animation.convert_path'] = '/fs1/masad/anaconda3/envs/resolve_statistics/bin/magick'
 #'{0}/magick'.format(os.path.dirname(which('python')))
 
+def use_resolvea(path_to_file):
+    resolve_live18 = pd.read_csv(path_to_raw + 'RESOLVE_liveJune2018.csv',
+                                delimiter=',')
+    #956 galaxies
+    resolve_A = resolve_live18.loc[(resolve_live18.f_a.values == 1) & 
+                                    (resolve_live18.grpcz.values >= 4500) & 
+                                    (resolve_live18.grpcz.values <= 7000) &
+                                    (resolve_live18.absrmag.values <= -17.33)] 
 
-#RESOLVE = pd.read_csv(path_to_interim + 'RESOLVE_formatted.txt',delimiter='\t')
-#RESOLVE_A = RESOLVE[RESOLVE['Name'].str.contains("rs")] #969
-#RESOLVE_B = RESOLVE[RESOLVE['Name'].str.contains("rf")] #402
+    volume = 13172.384 #Survey volume without buffer [Mpc/h]^3
+    cvar = 0.30
+    return resolve_A,volume,cvar
 
-resolve_live18 = pd.read_csv(path_to_raw + 'RESOLVE_liveJune2018.csv',delimiter=',')
-#956 galaxies
-resolve_A = resolve_live18.loc[(resolve_live18.f_a.values == 1) & \
-                               (resolve_live18.grpcz.values > 4500) & \
-                               (resolve_live18.grpcz.values < 7000) & \
-                               (resolve_live18.absrmag.values < -17.33)]
+def use_resolveb(path_to_file):
+    resolve_live18 = pd.read_csv(path_to_raw + 'RESOLVE_liveJune2018.csv',
+                                delimiter=',')
+    #487
+    resolve_B = resolve_live18.loc[(resolve_live18.f_b.values == 1) & 
+                                    (resolve_live18.grpcz.values >= 4500) & 
+                                    (resolve_live18.grpcz.values <= 7000) & 
+                                    (resolve_live18.absrmag.values <= -17)]
 
-v_resolveA = 13172.384 #Survey volume without buffer [Mpc/h]^3
-cvar_resolveA = 0.30
+    volume = 4709.8373#*2.915 #Survey volume without buffer [Mpc/h]^3
+    cvar = 0.58
+    return resolve_B,volume,cvar
 
-#487
-resolve_B = resolve_live18.loc[(resolve_live18.f_b.values == 1) & \
-                               (resolve_live18.grpcz.values > 4500) & \
-                               (resolve_live18.grpcz.values < 7000) & \
-                               (resolve_live18.absrmag.values < -17)]
+def use_eco(path_to_file):
+    columns = ['name', 'radeg', 'dedeg', 'cz', 'grpcz', 'absrmag', 'logmstar',
+                'logmgas', 'grp', 'grpn', 'logmh', 'logmh_s', 'fc',
+                'grpmb', 'grpms']
+    # 13878 galaxies
+    eco_buff = pd.read_csv(path_to_file,delimiter=",", header=0,
+                            usecols=columns)
+    # 6456 galaxies                       
+    eco_nobuff = eco_buff.loc[(eco_buff.grpcz.values >= 3000) & 
+                                (eco_buff.grpcz.values <= 7000) & 
+                                (eco_buff.absrmag.values <= -17.33)]
 
-v_resolveB = 4709.8373#*2.915 #Survey volume without buffer [Mpc/h]^3
-cvar_resolveB = 0.58
+    volume = 151829.26 # Survey volume without buffer [Mpc/h]^3
+    cvar = 0.125
+    return eco_nobuff,volume,cvar
 
-logM_resolve  = resolve_B.logmstar.values  #Read stellar masses
-nbins_resolve = 10  #Number of bins to divide data into
-V_resolve = v_resolveB 
-#Unnormalized histogram and bin edges
-Phi_resolve,edg_resolve = np.histogram(logM_resolve,bins=nbins_resolve)  
-dM_resolve = edg_resolve[1] - edg_resolve[0]  #Bin width
-Max_resolve = 0.5*(edg_resolve[1:]+edg_resolve[:-1])  #Mass axis i.e. bin centers
-#Normalized to volume and bin width
-err_poiss = np.sqrt(Phi_resolve)/(V_resolve*dM_resolve)
-err_cvar = 0.58/(V_resolve*dM_resolve)
-err_tot = np.sqrt(err_cvar**2 + err_poiss**2)
+def diff_smf(mstar_arr, volume, cvar_err, h1_bool):
+    """Calculates differential stellar mass function given stellar masses."""
+    
+    if not h1_bool:
+        # changing from h=0.7 to h=1
+        logmstar_arr = np.log10((10**mstar_arr) / 1.429)
+    else:
+        logmstar_arr = np.log10(mstar_arr)
+    # Unnormalized histogram and bin edges
+    bins = np.linspace(8.9,11.8,12)
+    phi, edg = np.histogram(logmstar_arr, bins=bins)  # paper used 17 bins
+    dm = edg[1] - edg[0]  # Bin width
+    maxis = 0.5 * (edg[1:] + edg[:-1])  # Mass axis i.e. bin centers
+    # Normalized to volume and bin width
+    err_poiss = np.sqrt(phi) / (volume * dm)
+    err_cvar = cvar_err / (volume * dm)
+    err_tot = np.sqrt(err_cvar**2 + err_poiss**2)
+    phi = phi / (volume * dm)  # not a log quantity
+    return maxis, phi, err_tot, bins
 
-Phi_resolve = Phi_resolve/(V_resolve*dM_resolve)  
+path_to_eco = path_to_raw + "eco_all.csv"
+eco_nobuff, volume, cvar = use_eco(path_to_eco)
+eco_nobuff_mstellar = eco_nobuff.logmstar.values
+maxis, phi, err, bins = diff_smf(eco_nobuff_mstellar, volume, cvar, False)
 
 fig1 = plt.figure(figsize=(10,8))
 plt.yscale('log')
 plt.xlim(8.9,11.5)
-plt.errorbar(Max_resolve,Phi_resolve,yerr=err_tot,fmt="rs--",    
+plt.errorbar(maxis,phi,yerr=err,fmt="rs--",    
     linewidth=2,   # width of plot line
     elinewidth=0.5,# width of error bar line
     ecolor='k',    # color of error bar
@@ -87,60 +122,7 @@ plt.errorbar(Max_resolve,Phi_resolve,yerr=err_tot,fmt="rs--",
 plt.xlabel(r'$\log(M_\star\,/\,M_\odot)$')
 plt.ylabel(r'$\Phi\,/\,\mathrm{dex}^{-1}\,\mathrm{Mpc}^{-3}$')
 plt.title('Stellar mass function')
-###############################################################################
-###COSMIC VARIANCE
-#eco = pd.read_csv(path_to_interim + 'ECO_DR1.txt',delimiter='\s+',header=None,usecols=\
-#                            [0,1,2,3,4,5,13,15],\
-#                            names=['ECOID','RA','DEC','velocity','M_r',\
-#                                   'log_stellarmass','groupcz',\
-#                                   'log_halogroupmass'])
-#    
-#dec_slices = np.arange(-1,49.85,2.5) #slices of 2.5 degrees width
-#minz_resB = np.min(resolve_B.cz/(3*10**5))
-#maxz_resB = np.max(resolve_B.cz/(3*10**5))
-#mincz_resB = 4500
-#maxcz_resB = 7000
-#H_0 = 70
-#slice_area = 2.5*75 #deg^2
-#deg2_in_sphere = 41252.96 #deg^2
-#
-#num_in_slice = []
-#z = []
-#for index,value in enumerate(dec_slices):
-#    if index == 20:
-#        break
-#    counter = 0
-#    RA_arr = []
-#    DEC_arr = []
-#    for index2,galaxy in enumerate(eco.ECOID.values):
-#        if 134.99 < eco.RA.values[index2] < 209.99: #fixed RA span of 75 degrees
-#            if value < eco.DEC.values[index2] < dec_slices[index+1]:
-#                if minz_resB < (eco.velocity.values[index2]/(3*10**5)) < maxz_resB:
-#                    z.append(eco.velocity.values[index2]/(3*10**5))
-#                    RA_arr.append(eco.RA.values[index2])
-#                    DEC_arr.append(eco.DEC.values[index2])
-#                    counter += 1
-#    fig = plt.figure(figsize=(10,8))
-#    ax = plt.gca()
-#    plt.ylim(-1,49.85)
-#    plt.xlim(0,75)
-#    plt.scatter(RA_arr,DEC_arr)
-#    plt.xlabel('RA (deg)')
-#    plt.ylabel('DEC (deg)')
-#    num_in_slice.append(counter)
-#num_in_slice = np.array(num_in_slice)
-#
-#inner_vol = (4/3)*np.pi*((mincz_resB/H_0)**3)
-#outer_vol = (4/3)*np.pi*((maxcz_resB/H_0)**3)
-#vol_sphere = outer_vol-inner_vol #Mpc^3
-#
-#vol_slice = (slice_area/deg2_in_sphere)*vol_sphere
-#gal_dens = num_in_slice/vol_slice
-#cosmic_variance = (np.std(gal_dens)*100)
-#print('Cosmic variance: {0}%'.format(np.round(cosmic_variance,2)))
-###############################################################################
 
-#%%
 counter = 0
 nbins = 10
 Volume_sim = 130**3
@@ -148,28 +130,36 @@ Mhalo_characteristic = np.arange(11.5,13.0,0.1) #13.0 not included
 Mstellar_characteristic = np.arange(9.5,11.0,0.1) #11.0 not included
 Mlow_slope = np.arange(0.35,0.50,0.01)[:-1] #0.5 included by default
 Mhigh_slope = np.arange(0.50,0.65,0.01)[:-1]
-Mstellar_scatter = np.arange(0.02,0.095,0.005)
+#testing with extreme scatter numbers from ECO's MCMC run
+Mstellar_scatter = np.linspace(10,800,15) 
+
+###Models
+model1 = PrebuiltSubhaloModelFactory('behroozi10',redshift=0.0186,\
+                                        prim_haloprop_key='halo_macc')
+model2 = PrebuiltSubhaloModelFactory('behroozi10',redshift=0.0186,\
+                                        prim_haloprop_key='halo_macc')
+model3 = PrebuiltSubhaloModelFactory('behroozi10',redshift=0.0186,\
+                                        prim_haloprop_key='halo_macc')
+model4 = PrebuiltSubhaloModelFactory('behroozi10',redshift=0.0186,\
+                                        prim_haloprop_key='halo_macc')
+model5 = PrebuiltSubhaloModelFactory('behroozi10',redshift=0.0186,\
+                                        prim_haloprop_key='halo_macc')
+
+###Halocats
+halocat1 = CachedHaloCatalog(fname=halo_catalog)
+halocat2 = CachedHaloCatalog(fname=halo_catalog)
+halocat3 = CachedHaloCatalog(fname=halo_catalog)
+halocat4 = CachedHaloCatalog(fname=halo_catalog)
+halocat5 = CachedHaloCatalog(fname=halo_catalog)
+
+###Populate mocks
+model1.populate_mock(halocat1)
+model2.populate_mock(halocat2)
+model3.populate_mock(halocat3)
+model4.populate_mock(halocat4)
+model5.populate_mock(halocat5)
 
 def gals(Mhalo_value,Mstellar_value,Mlow_slope,Mhigh_slope,Mstellar_scatter):   
-    ###Models
-    model1 = PrebuiltSubhaloModelFactory('behroozi10',redshift=0.0186,\
-                                         prim_haloprop_key='halo_macc')
-    model2 = PrebuiltSubhaloModelFactory('behroozi10',redshift=0.0186,\
-                                         prim_haloprop_key='halo_macc')
-    model3 = PrebuiltSubhaloModelFactory('behroozi10',redshift=0.0186,\
-                                         prim_haloprop_key='halo_macc')
-    model4 = PrebuiltSubhaloModelFactory('behroozi10',redshift=0.0186,\
-                                         prim_haloprop_key='halo_macc')
-    model5 = PrebuiltSubhaloModelFactory('behroozi10',redshift=0.0186,\
-                                         prim_haloprop_key='halo_macc')
-    
-    ###Halocats
-    halocat1 = CachedHaloCatalog(fname=halo_catalog)
-    halocat2 = CachedHaloCatalog(fname=halo_catalog)
-    halocat3 = CachedHaloCatalog(fname=halo_catalog)
-    halocat4 = CachedHaloCatalog(fname=halo_catalog)
-    halocat5 = CachedHaloCatalog(fname=halo_catalog)
-
     ###Parameter values
     model1.param_dict['smhm_m1_0'] = Mhalo_value
     model2.param_dict['smhm_m0_0'] = Mstellar_value
@@ -177,13 +167,12 @@ def gals(Mhalo_value,Mstellar_value,Mlow_slope,Mhigh_slope,Mstellar_scatter):
     model4.param_dict['smhm_delta_0'] = Mhigh_slope
     model5.param_dict['u’scatter_model_param1'] = Mstellar_scatter
     
-    ###Populate mocks
-    model1.populate_mock(halocat1)
-    model2.populate_mock(halocat2)
-    model3.populate_mock(halocat3)
-    model4.populate_mock(halocat4)
-    model5.populate_mock(halocat5)
-    
+    model1.mock.populate()
+    model2.mock.populate()
+    model3.mock.populate()
+    model4.mock.populate()
+    model5.mock.populate()
+
     #Applying RESOLVE-B stellar mass limit
     sample_mask1 = model1.mock.galaxy_table['stellar_mass'] >= 10**8.7
     sample_mask2 = model2.mock.galaxy_table['stellar_mass'] >= 10**8.7
@@ -265,7 +254,7 @@ def make_animation(i,j,k,l,m):
     
     for ax in [ax1,ax2,ax3,ax4,ax5]:
         ax.clear()
-        SMF_RESOLVEB = ax.errorbar(Max_resolve,Phi_resolve,yerr=err_tot,\
+        SMF_RESOLVEB = ax.errorbar(maxis,phi,yerr=err,\
                                   fmt="rs--",linewidth=2,elinewidth=0.5,\
                                   ecolor='k',capsize=2,capthick=1.5,\
                                   markersize='3')
@@ -329,5 +318,4 @@ anim = animation.FuncAnimation(plt.gcf(), make_animation, \
 plt.tight_layout()
 print('Saving animation')
 os.chdir(path_to_figures)
-anim.save('SMF_RB_macc_2.gif',writer='imagemagick',fps=1)
-
+anim.save('SMF_ECO_macc_MCMC.gif',writer='imagemagick',fps=1)
