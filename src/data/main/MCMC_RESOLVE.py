3ae12dc0579dea92f1c40952dfb88b41652f54ab
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2019-04-09 17:32:07 -0500

    running resolve B

--- src/data/main/MCMC_RESOLVE.py
+++ src/data/main/MCMC_RESOLVE.py
@@ -1,45 +1,31 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Tue Feb  4 09:39:34 2019.
+{This script carries out an MCMC analysis to parametrize the RESOLVE-B SMHM}
+"""
 
-@author: asadm2
-"""
+# Built-in/Generic Imports
+import time
+
+# Libs
 from halotools.empirical_models import PrebuiltSubhaloModelFactory
-# from halotools.empirical_models import Moster13SmHm
 from halotools.sim_manager import CachedHaloCatalog
 from cosmo_utils.utils import work_paths as cwpaths
-# import matplotlib
-# matplotlib.use('Agg')
-# import matplotlib.pyplot as plt
-# plt.ioff()
-from matplotlib import rc
+from multiprocessing import Pool
 import pandas as pd
 import numpy as np
 import emcee
 import math
-# import os
 
-
-def num_bins(data_arr):
-    """Returns number of appropriate bins given data to be binned."""
-    q75, q25 = np.percentile(data_arr, [75, 25])
-    iqr = q75 - q25
-    num_points = len(data_arr)
-    h = 2 * iqr * (num_points**(-1 / 3))
-    n_bins = math.ceil((max(data_arr) - min(data_arr)) / h)  # Round up number
-    return n_bins
-
+__author__ = '{Mehnaaz Asad}'
 
 def diff_smf(mstar_arr, volume, cvar_err, h1_bool):
     """Calculates differential stellar mass function given stellar masses."""
+    
     if not h1_bool:
         # changing from h=0.7 to h=1
         logmstar_arr = np.log10((10**mstar_arr) / 1.429)
     else:
         logmstar_arr = np.log10(mstar_arr)
-    bins = np.linspace(8.9, 11.5, 11)
-    # nbins = num_bins(logmstar_arr)  #Number of bins to divide data into
+    bins = np.linspace(8.7, 11.5, 12)
     # Unnormalized histogram and bin edges
     phi, edg = np.histogram(logmstar_arr, bins=bins)  # paper used 17 bins
     dm = edg[1] - edg[0]  # Bin width
@@ -48,21 +34,11 @@ def diff_smf(mstar_arr, volume, cvar_err, h1_bool):
     err_poiss = np.sqrt(phi) / (volume * dm)
     err_cvar = cvar_err / (volume * dm)
     err_tot = np.sqrt(err_cvar**2 + err_poiss**2)
-
     phi = phi / (volume * dm)  # not a log quantity
-
     return maxis, phi, err_tot, bins
-
 
 def populate_mock(theta, model):
     """Populates halo catalog with galaxies given SMHM parameters and model."""
-    # halocat = CachedHaloCatalog(fname=halo_catalog,update_cached_fname=True)
-    # z_model = np.median(resolve_live18.grpcz.values)/(3*10**5)
-    # if model_name=='Moster':
-    #     model = Moster13SmHm(prim_haloprop_key='halo_macc')
-    # elif model_name=='Behroozi':
-    #     model = PrebuiltSubhaloModelFactory('behroozi10',redshift=z_model,\
-    #                                  prim_haloprop_key='halo_macc')
 
     mhalo_characteristic, mstellar_characteristic, mlow_slope, mhigh_slope,\
         mstellar_scatter = theta
@@ -79,7 +55,6 @@ def populate_mock(theta, model):
     gals_df = gals.to_pandas()
 
     return gals_df
-
 
 def chi_squared(data_y, model_y, err_data):
     """Calculates chi squared."""
@@ -115,12 +90,8 @@ path_to_interim = dict_of_paths['int_dir']
 path_to_figures = dict_of_paths['plot_dir']
 halo_catalog = '/home/asadm2/.astropy/cache/halotools/halo_catalogs/'\
                'vishnu/rockstar/vishnu_rockstar_test.hdf5'
-# halo_catalog = '/Users/asadm2/Desktop/vishnu_rockstar_test.hdf5'
-chain_fname = path_to_proc + 'emcee_SMFRB.dat'
-
-# Formatting for plots and animation
-rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']}, size=18)
-rc('text', usetex=True)
+# halo_catalog = path_to_raw + 'vishnu_rockstar_test.hdf5'
+chain_fname = path_to_proc + 'emcee_resolveB_mp_r2.dat'
 
 columns = ['name', 'radeg', 'dedeg', 'cz', 'grpcz', 'absrmag', 'logmstar',
            'logmgas', 'grp', 'grpn', 'grpnassoc', 'logmh', 'logmh_s', 'fc',
@@ -130,20 +101,23 @@ columns = ['name', 'radeg', 'dedeg', 'cz', 'grpcz', 'absrmag', 'logmstar',
 resolve_live18 = pd.read_csv(path_to_raw + "RESOLVE_liveJune2018.csv",
                              delimiter=",", header=0, usecols=columns)
 
-# 956 galaxies
-resolve_A = resolve_live18.loc[(resolve_live18.f_a.values == 1) &
-                               (resolve_live18.grpcz.values > 4500) &
-                               (resolve_live18.grpcz.values < 7000) &
-                               (resolve_live18.absrmag.values < -17.33)]
+# 956 - cz, 634 - grpcz
+resolve_A = resolve_live18.loc[(resolve_live18.f_a.values == 1) & \
+    (resolve_live18.grpcz.values > 4500) & \
+        (resolve_live18.grpcz.values < 7000) & \
+            (resolve_live18.absrmag.values < -17.33) & \
+                (resolve_live18.logmstar.values >= 8.9)]
+
 
 v_resolveA = 13172.384  # Survey volume without buffer [Mpc/h]^3
 cvar_resolveA = 0.30
 
-# 487
-resolve_B = resolve_live18.loc[(resolve_live18.f_b.values == 1) &
-                               (resolve_live18.grpcz.values > 4500) &
-                               (resolve_live18.grpcz.values < 7000) &
-                               (resolve_live18.absrmag.values < -17)]
+# 487 - cz, 369 - grpcz
+resolve_B = resolve_live18.loc[(resolve_live18.f_b.values == 1) & \
+    (resolve_live18.grpcz.values > 4500) & \
+        (resolve_live18.grpcz.values < 7000) & \
+            (resolve_live18.absrmag.values < -17) & \
+                (resolve_live18.logmstar.values >= 8.7)]
 
 v_resolveB = 4709.8373  # *2.915 #Survey volume without buffer [Mpc/h]^3
 cvar_resolveB = 0.58
@@ -165,68 +139,102 @@ model = PrebuiltSubhaloModelFactory('behroozi10', redshift=z_model,
                                     prim_haloprop_key='halo_macc')
 model.populate_mock(halocat)
 
+# ensure reproducibility
+rseed = 12
+np.random.seed(rseed)
+
 behroozi10_param_vals = [12.35,10.72,0.44,0.57,0.15]
 nwalkers = 250
 ndim = 5
-p0 = behroozi10_param_vals + np.random.rand(ndim*nwalkers).reshape((nwalkers,\
-                                                                    ndim))
-# p0 = np.random.rand(ndim * nwalkers).reshape((nwalkers, ndim))
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(phi_resolveB, \
-          err_tot_B))
-# sampler.run_mcmc(p0, 50)
-nsteps = 500
+p0 = behroozi10_param_vals + 0.1*np.random.rand(ndim*nwalkers).\
+    reshape((nwalkers,ndim)) 
+
+with Pool(20) as pool:
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(phi_resolveB, \
+          err_tot_B),pool=pool)
+    start = time.time()
+    sampler.run_mcmc(p0, 500)
+    end = time.time()
+    multi_time = end - start
+    print("Multiprocessing took {0:.1f} seconds".format(multi_time))
+
+print("Writing raw chain to file")
+data = sampler.chain
+with open(path_to_proc + 'resolveB_mcmc_raw.txt', 'w') as outfile:
+    # outfile.write('# Array shape: {0}\n'.format(sampler.chain.shape))
+    for data_slice in data:
+        np.savetxt(outfile, data_slice, fmt='%-7.2f')
+        outfile.write('# New slice\n')
+
+print("Writing flattened chain to file")
+chain = sampler.flatchain
 f = open(chain_fname, "w")
+for idx in range(len(chain)):
+    f.write(str(chain[idx]).strip("[]"))
+    f.write("\n")
 f.close()
+'''
+nsteps = 5
+# f = open(chain_fname, "w")
+# f.close()
 
 for i,result in enumerate(sampler.sample(p0, iterations=nsteps, storechain=False)):
-    position = result[0]
+    # position = result[0]
     print("Iteration number {0} of {1}".format(i+1,nsteps))
-    f = open(chain_fname, "a")
-    for k in range(position.shape[0]):
-        f.write(str(position[k]).strip("[]"))
-        f.write("\n")
-    f.close()
-
-# np.savetxt(chain_fname,sampler.flatchain)
-# emcee_table = pd.read_csv('/Users/asadm2/Documents/Grad_School/Research/Repositories/resolve_statistics/chain.dat',names=['mhalo_c','mstellar_c','lowmass_slope','highmass_slope','scatter'],sep='\s+',dtype=np.float64)
+    # f = open(chain_fname, "a")
+    # for k in range(position.shape[0]):
+    #     f.write(str(position[k]).strip("[]"))
+    #     f.write("\n")
+    # f.close()
 '''
-### Plot SMFs
+'''
+resolve_A_grp = resolve_live18.loc[(resolve_live18.f_a.values == 1) & \
+    (resolve_live18.grpcz.values > 4500) & \
+        (resolve_live18.grpcz.values < 7000) & \
+            (resolve_live18.absrmag.values < -17.33) & \
+                (resolve_live18.logmstar.values >= 8.9)]
+
+resolve_B_grp = resolve_live18.loc[(resolve_live18.f_b.values == 1) & \
+    (resolve_live18.grpcz.values > 4500) & \
+        (resolve_live18.grpcz.values < 7000) & \
+            (resolve_live18.absrmag.values < -17) & \
+                (resolve_live18.logmstar.values >= 8.7)]
+
+resa_m_stellar_grp = resolve_A_grp.logmstar.values
+max_resolveA_grp, phi_resolveA_grp, err_tot_A_grp, bins_A_grp = diff_smf(resa_m_stellar_grp,
+                                                         v_resolveA,
+                                                         cvar_resolveA, False)
+resb_m_stellar_grp = resolve_B_grp.logmstar.values
+max_resolveB_grp, phi_resolveB_grp, err_tot_B_grp, bins_B_grp = diff_smf(resb_m_stellar_grp,
+                                                         v_resolveB,
+                                                         cvar_resolveB, False)
 fig1 = plt.figure(figsize=(10,10))
-plt.errorbar(max_resolveB,phi_resolveB,yerr=err_tot_B,\
-             color='#921063',fmt='--s',ecolor='#921063',markersize=4,capsize=5,\
-             capthick=0.5,label='RESOLVE B')
+plt.errorbar(max_resolveA_grp,phi_resolveA_grp,yerr=err_tot_A_grp,\
+    color='#1baeab',fmt='--s',ecolor='#1baeab',markersize=4,capsize=5,\
+        capthick=0.5,label=r'group cz')
 plt.errorbar(max_resolveA,phi_resolveA,yerr=err_tot_A,\
-             color='#442b88',fmt='--s',ecolor='#442b88',markersize=4,capsize=5,\
-             capthick=0.5,label='RESOLVE A')
+    color='#f6a631',fmt='--s',ecolor='#f6a631',markersize=4,capsize=5,\
+        capthick=0.5,label=r'galaxy cz')
+
+plt.xlabel(r'\boldmath$\log\ M_{\star} \left[M_\odot \right]$',size=15)
+plt.ylabel(r'$\Phi\,/\,\mathrm{dex}^{-1}\,\mathrm{Mpc}^{-3}$',size=15)
 plt.yscale('log')
-xlim_left,xlim_right = plt.xlim()
-# plt.axvline(x=8.7,color='#921063')
-# plt.axvline(x=8.9,color='#442b88')
-# plt.axvspan(xlim_left, 8.7, color='gray', alpha=0.7, lw=0)
-plt.xlabel(r'\boldmath$\log_{10}\ M_\star \left[M_\odot \right]$')
-plt.ylabel(r'\boldmath$\Phi \left[\mathrm{dex}^{-1}\,\mathrm{Mpc}^{-3}\,\
-mathrm{h}^{-1} \right]$')
+plt.title(r'\boldmath RESOLVE-A',size=15)
 plt.legend(loc='best',prop={'size': 10})
 plt.show()
 
 fig2 = plt.figure(figsize=(10,10))
+plt.errorbar(max_resolveB_grp,phi_resolveB_grp,yerr=err_tot_B_grp,\
+    color='#1baeab',fmt='--s',ecolor='#1baeab',markersize=4,capsize=5,\
+        capthick=0.5,label=r'group cz')
 plt.errorbar(max_resolveB,phi_resolveB,yerr=err_tot_B,\
-             color='#921063',fmt='--s',ecolor='#921063',markersize=4,capsize=5,\
-             capthick=0.5,label='RESOLVE B')
-plt.errorbar(max_resolveA,phi_resolveA,yerr=err_tot_A,\
-             color='#442b88',fmt='--s',ecolor='#442b88',markersize=4,capsize=5,\
-             capthick=0.5,label='RESOLVE A')
+    color='#f6a631',fmt='--s',ecolor='#f6a631',markersize=4,capsize=5,\
+        capthick=0.5,label=r'galaxy cz')
 
-plt.plot(Max,Phi,color='k',linestyle='-',label='Vishnu')
-
+plt.xlabel(r'\boldmath$\log\ M_{\star} \left[M_\odot \right]$',size=15)
+plt.ylabel(r'$\Phi\,/\,\mathrm{dex}^{-1}\,\mathrm{Mpc}^{-3}$',size=15)
 plt.yscale('log')
-plt.ylim(10**-5,10**-1)
-# plt.axvline(x=8.7,color='#921063')
-# plt.axvline(x=8.9,color='#442b88')
-# plt.axvspan(xlim_left, 8.7, color='gray', alpha=0.7, lw=0)
-plt.xlabel(r'\boldmath$\log_{10}\ M_\star \left[M_\odot \right]$')
-plt.ylabel(r'\boldmath$\Phi \left[\mathrm{dex}^{-1}\,\mathrm{Mpc}^{-3}\,\
-mathrm{h}^{-1} \right]$')
+plt.title(r'\boldmath RESOLVE-B',size=15)
 plt.legend(loc='best',prop={'size': 10})
 plt.show()
 '''
