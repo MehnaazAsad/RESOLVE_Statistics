f1203d7efa687c56621e1e81e8b936422ef95213
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2019-09-03 16:04:56 -0500

    added measuring and plotting mock colour SMF functions

--- src/data/main/smf_analysis_mocks.py
+++ src/data/main/smf_analysis_mocks.py
@@ -1,5 +1,6 @@
 """
-{This script calculates cosmic variance from mocks}
+{This script calculates cosmic variance from mocks and plots SMF for red and 
+ blue galaxies from mocks}
 """
 
 # Libs
@@ -19,6 +20,45 @@ __author__ = '{Mehnaaz Asad}'
 rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']}, size=10)
 rc('text', usetex=True)
 
+def survey_definitions():
+    
+    eco = {
+        "mock_name": "ECO",
+        "num_mocks": 8,
+        "min_cz": 3000,
+        "max_cz": 7000,
+        "mag_limit": -17.33,
+        "mstar_limit": 8.9,
+        "volume": 151829.26 # Survey volume without buffer [Mpc/h]^3
+    }
+
+    resolvea = {
+        "mock_name": "A",
+        "num_mocks": 59,
+        "min_cz": 4500,
+        "max_cz": 7000,
+        "mag_limit": -17.33,
+        "mstar_limit": 8.9,
+        "volume": 13172.384 # Survey volume without buffer [Mpc/h]^3
+    }
+
+    resolveb = {
+        "mock_name": "B",
+        "num_mocks": 104,
+        "min_cz": 4500,
+        "max_cz": 7000,
+        "mag_limit": -17,
+        "mstar_limit": 8.7,
+        "volume": 4709.8373 # Survey volume without buffer [Mpc/h]^3
+    }
+
+    if survey == 'eco':
+        return eco
+    elif survey == 'resolvea':
+        return resolvea
+    elif survey == 'resolveb':
+        return resolveb
+    
 def reading_catls(filename, catl_format='.hdf5'):
     """
     Function to read ECO/RESOLVE catalogues.
@@ -117,16 +157,17 @@ def read_data(path_to_file, survey):
     if survey == 'eco':
         columns = ['name', 'radeg', 'dedeg', 'cz', 'grpcz', 'absrmag', 
                     'logmstar', 'logmgas', 'grp', 'grpn', 'logmh', 'logmh_s', 
-                    'fc', 'grpmb', 'grpms']
+                    'fc', 'grpmb', 'grpms', 'modelu_rcorr', 'umag', 'rmag']
 
         # 13878 galaxies
         eco_buff = pd.read_csv(path_to_file,delimiter=",", header=0, \
             usecols=columns)
 
         # 6456 galaxies                       
-        catl = eco_buff.loc[(eco_buff.grpcz.values >= 3000) & \
-            (eco_buff.grpcz.values <= 7000) & (eco_buff.absrmag.values <= -17.33) &\
-                (eco_buff.logmstar.values >= 8.9)]
+        catl = eco_buff.loc[(eco_buff.grpcz.values >= 3000) & 
+            (eco_buff.grpcz.values <= 7000) & 
+            (eco_buff.absrmag.values <= -17.33) &
+            (eco_buff.logmstar.values >= 8.9)]
 
         volume = 151829.26 # Survey volume without buffer [Mpc/h]^3
         cvar = 0.125
@@ -135,7 +176,8 @@ def read_data(path_to_file, survey):
     elif survey == 'resolvea' or survey == 'resolveb':
         columns = ['name', 'radeg', 'dedeg', 'cz', 'grpcz', 'absrmag', 
                     'logmstar', 'logmgas', 'grp', 'grpn', 'grpnassoc', 'logmh', 
-                    'logmh_s', 'fc', 'grpmb', 'grpms', 'f_a', 'f_b']
+                    'logmh_s', 'fc', 'grpmb', 'grpms', 'f_a', 'f_b', 
+                    'modelu_rcorr']
         # 2286 galaxies
         resolve_live18 = pd.read_csv(path_to_file, delimiter=",", header=0, \
             usecols=columns)
@@ -205,8 +247,8 @@ def diff_smf(mstar_arr, volume, cvar_err, h1_bool):
         logmstar_arr = mstar_arr
     if survey == 'eco' or survey == 'resolvea':
         bin_min = np.round(np.log10((10**8.9) / 2.041), 1)
-        bin_max = np.round(np.log10((10**11.3) / 2.041), 1)
-        bins = np.linspace(bin_min, bin_max, 12)
+        bin_max = np.round(np.log10((10**11.8) / 2.041), 1)
+        bins = np.linspace(bin_min, bin_max, 6)
     elif survey == 'resolveb':
         bin_min = np.round(np.log10((10**8.7) / 2.041), 1)
         bin_max = np.round(np.log10((10**11.8) / 2.041), 1)
@@ -246,14 +288,12 @@ def diff_bmf(mass_arr, volume, cvar_err, sim_bool, h1_bool):
     phi = counts / (volume * dm)  # not a log quantity
     return maxis, phi, err_poiss, bins, counts
 
-def measure_mock_mf(survey, path):
+def measure_mock_total_mf(path):
     """
     Calculate error in data SMF from mocks
 
     Parameters
     ----------
-    survey: string
-        Name of survey
     path: string
         Path to mock catalogs
 
@@ -266,48 +306,24 @@ def measure_mock_mf(survey, path):
     err_blue: array
         Standard deviation of phi values between all mocks and for blue galaxies
     """
-
-    if survey == 'eco':
-        mock_name = 'ECO'
-        num_mocks = 8
-        min_cz = 3000
-        max_cz = 7000
-        mag_limit = -17.33
-        mstar_limit = 8.9
-        volume = 151829.26 # Survey volume without buffer [Mpc/h]^3
-    elif survey == 'resolvea':
-        mock_name = 'A'
-        num_mocks = 59
-        min_cz = 4500
-        max_cz = 7000
-        mag_limit = -17.33
-        mstar_limit = 8.9
-        volume = 13172.384  # Survey volume without buffer [Mpc/h]^3 
-    elif survey == 'resolveb':
-        mock_name = 'B'
-        num_mocks = 104
-        min_cz = 4500
-        max_cz = 7000
-        mag_limit = -17
-        mstar_limit = 8.7
-        volume = 4709.8373  # Survey volume without buffer [Mpc/h]^3
-
     phi_smf_arr = []
     phi_bmf_arr = []
     max_smf_arr = []
     max_bmf_arr = []
     err_smf_arr = []
     err_bmf_arr = []
-    for num in range(num_mocks):
+
+    for num in range(survey_dict["num_mocks"]):
         filename = path + '{0}_cat_{1}_Planck_memb_cat.hdf5'.format(
-            mock_name, num)
+            survey_dict["mock_name"], num)
         mock_pd = reading_catls(filename) 
 
         # Using the same survey definition as in mcmc smf i.e excluding the 
         # buffer
-        mock_pd = mock_pd.loc[(mock_pd.cz.values >= min_cz) & \
-            (mock_pd.cz.values <= max_cz) & (mock_pd.M_r.values <= mag_limit) &\
-                (mock_pd.logmstar.values >= mstar_limit)]
+        mock_pd = mock_pd.loc[(mock_pd.cz.values >= survey_dict["min_cz"]) & \
+            (mock_pd.cz.values <= survey_dict["max_cz"]) & \
+            (mock_pd.M_r.values <= survey_dict["mag_limit"]) & \
+            (mock_pd.logmstar.values >= survey_dict["mstar_limit"])]
 
         # Convert mhI and logmstar masses to h=1.0 assuming h^-2 dependence
         logmstar_arr = np.log10((10**mock_pd.logmstar.values) / 2.041)
@@ -318,9 +334,9 @@ def measure_mock_mf(survey, path):
         # Measure SMF of mock using diff_smf function
         # Volume in h=1 since stellar masses have been changed to be in h=1
         max_smf, phi_smf, err_smf, bins_smf, counts_smf = \
-            diff_smf(logmstar_arr, volume, 0, True)
+            diff_smf(logmstar_arr, survey_dict["volume"], 0, True)
         max_bmf, phi_bmf, err_bmf, bins_bmf, counts_bmf = \
-            diff_bmf(logmbary_arr, volume, 0, False, True)
+            diff_bmf(logmbary_arr, survey_dict["volume"], 0, False, True)
 
         phi_smf_arr.append(phi_smf)
         phi_bmf_arr.append(phi_bmf)
@@ -335,9 +351,89 @@ def measure_mock_mf(survey, path):
     elif mf_type == 'bmf':
         return max_bmf_arr, phi_bmf_arr, err_bmf_arr
 
+def measure_mock_colour_mf(path):
+    """
+    Assign colour label to data
+
+    Parameters
+    ----------
+    path: string
+        Path to mock catalogs
+
+    Returns
+    ---------
+    catl: pandas Dataframe
+        Data catalog with colour label assigned as new column
+    """
+    cols = survey_dict["num_mocks"]
+    rows = 3 # store mass, phi, error measurements from SMF
+    len_arr = 5 # number of bins in SMF
+    smf_red = [[[0 for k in range(len_arr)] for j in range(cols)] 
+        for i in range(rows)]
+    smf_blue = [[[0 for k in range(len_arr)] for j in range(cols)] 
+        for i in range(rows)]
+
+    for num in range(survey_dict["num_mocks"]):
+        filename = path + '{0}_cat_{1}_Planck_memb_cat.hdf5'.format(
+            survey_dict["mock_name"], num)
+        mock_pd = reading_catls(filename) 
+
+        # Using the same survey definition as in mcmc smf i.e excluding the 
+        # buffer
+        mock_pd = mock_pd.loc[(mock_pd.cz.values >= survey_dict["min_cz"]) & \
+            (mock_pd.cz.values <= survey_dict["max_cz"]) & \
+            (mock_pd.M_r.values <= survey_dict["mag_limit"]) & \
+            (mock_pd.logmstar.values >= survey_dict["mstar_limit"])]
+
+        logmstar_arr = mock_pd.logmstar.values
+        u_r_arr = mock_pd.u_r.values
+
+        colour_label_arr = np.empty(len(mock_pd), dtype='str')
+        for idx, value in enumerate(logmstar_arr):
+            # Divisions taken from Moffett et al. 2015 equation 1
+            if value <= 9.1:
+                if u_r_arr[idx] > 1.457:
+                    colour_label = 'R'
+                else:
+                    colour_label = 'B'
+
+            elif value > 9.1 and value < 10.1:
+                divider = 0.24 * value - 0.7
+                if u_r_arr[idx] > divider:
+                    colour_label = 'R'
+                else:
+                    colour_label = 'B'
+
+            elif value >= 10.1:
+                if u_r_arr[idx] > 1.7:
+                    colour_label = 'R'
+                else:
+                    colour_label = 'B'
+                
+            colour_label_arr[idx] = colour_label
+        
+        mock_pd['colour_label'] = colour_label_arr
+
+        max_red, phi_red, err_red, bins_red, counts_red = \
+            diff_smf(mock_pd.logmstar.loc[mock_pd.colour_label.values == 'R'],
+                survey_dict["volume"], 0, False)
+        max_blue, phi_blue, err_blue, bins_blue, counts_blue = \
+            diff_smf(mock_pd.logmstar.loc[mock_pd.colour_label.values == 'B'], 
+                survey_dict["volume"], 0, False)
+
+        smf_red[0][num] = max_red
+        smf_red[1][num] = phi_red
+        smf_red[2][num] = err_red
+        smf_blue[0][num] = max_blue
+        smf_blue[1][num] = phi_blue
+        smf_blue[2][num] = err_blue       
+
+    return smf_red, smf_blue
+
 def measure_cov_mat(mf_array):
 
     cov_mat = np.cov(mf_array)
+    print(cov_mat.shape)
     fig1 = plt.figure()
     ax = sns.heatmap(cov_mat, linewidth=0.5)
     plt.gca().invert_xaxis()
@@ -346,7 +442,7 @@ def measure_cov_mat(mf_array):
 def measure_corr_mat(mf_array):
 
     if mf_type == 'smf':
-        columns = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'] 
+        columns = ['1', '2', '3', '4', '5']#, '6', '7', '8', '9', '10', '11'] 
     elif mf_type == 'bmf':
         columns = ['1', '2', '3', '4', '5', '6', '7', '8']
 
@@ -382,18 +478,18 @@ def measure_corr_mat(mf_array):
             plt.title('RESOLVE-B BMF')
     plt.show()
 
-def plot_all_mf(max_mock, phi_mock, err_mock, max_data, phi_data, err_data, 
-    counts_data):
+def plot_total_mf(max_1, phi_1, err_1, max_2, phi_2, err_2, 
+    counts_2):
 
     fig4 = plt.figure()
-    for idx in range(len(phi_mock)):
-        plt.errorbar(max_mock[idx],phi_mock[idx],yerr=err_mock[idx],
+    for idx in range(len(phi_1)):
+        plt.errorbar(max_1[idx],phi_1[idx],yerr=err_1[idx],
         markersize=4,capsize=5,capthick=0.5)
-    for i in range(len(phi_data)):
-        text = plt.text(max_data[i], 10**-1.07, counts_data[i],
+    for i in range(len(phi_2)):
+        text = plt.text(max_2[i], 10**-1.07, counts_2[i],
         ha="center", va="center", color="k", size=7)
-    plt.errorbar(max_data,phi_data,yerr=err_data,markersize=4,capsize=5,
-    capthick=0.5, label='data',color='k',fmt='-s',ecolor='k')
+    plt.errorbar(max_2,phi_2,yerr=err_2,markersize=4,capsize=5, capthick=0.5, 
+        label='data',color='k',fmt='-s',ecolor='k')
     plt.xlabel(r'\boldmath$\log_{10}\ M_\star \left[\mathrm{M_\odot}\, \mathrm{h}^{-1} \right]$')
     plt.ylabel(r'\boldmath$\Phi \left[\mathrm{dex}^{-1}\,\mathrm{Mpc}^{-3}\,\mathrm{h}^{-3} \right]$')
     plt.yscale('log')
@@ -416,6 +512,37 @@ def plot_all_mf(max_mock, phi_mock, err_mock, max_data, phi_data, err_data,
 
     plt.legend(loc='best', prop={'size': 10})
     plt.show() 
+
+def plot_colour_mf(smf_1, smf_2):
+
+    fig5 = plt.figure()
+    for idx in range(len(smf_1[0])):
+        plt.errorbar(smf_1[0][idx],smf_1[1][idx],yerr=smf_1[2][idx],
+        markersize=4,capsize=5,capthick=0.5,color='r')
+    for idx in range(len(smf_2[0])):
+        plt.errorbar(smf_2[0][idx],smf_2[1][idx],yerr=smf_2[2][idx],
+        markersize=4,capsize=5,capthick=0.5,color='b')
+    plt.xlabel(r'\boldmath$\log_{10}\ M_\star \left[\mathrm{M_\odot}\, \mathrm{h}^{-1} \right]$')
+    plt.ylabel(r'\boldmath$\Phi \left[\mathrm{dex}^{-1}\,\mathrm{Mpc}^{-3}\,\mathrm{h}^{-3} \right]$')
+    plt.yscale('log')
+    
+    if survey == 'eco':
+        if mf_type == 'smf':
+            plt.title('ECO SMF')
+        elif mf_type == 'bmf':
+            plt.title('ECO BMF')
+    elif survey == 'resolvea':
+        if mf_type == 'smf':
+            plt.title('RESOLVE-A SMF')
+        if mf_type == 'bmf':
+            plt.title('RESOLVE-A BMF')
+    elif survey == 'resolveb':
+        if mf_type == 'smf':
+            plt.title('RESOLVE-B SMF')
+        if mf_type == 'bmf':
+            plt.title('RESOLVE-B BMF')
+
+    plt.show()
 
 def cosmic_vs_poisson(catl_file):
 
@@ -457,9 +584,11 @@ def main(args):
     """
     global survey
     global mf_type
+    global survey_dict
     
     survey = args.survey
     mf_type = args.mf_type
+    survey_dict = survey_definitions()
 
     # Paths
     dict_of_paths = cwpaths.cookiecutter_paths()
@@ -492,8 +621,11 @@ def main(args):
         max_data, phi_data, err_data, bins_data, counts_data = \
             diff_bmf(bary_mass_arr, volume, cvar, False, True)
 
-    print("Measuring mass function for mock")   
-    max_mock, phi_mock, err_mock  = measure_mock_mf(survey, path_to_mocks)
+    print("Measuring total mass function for mock")   
+    max_mock, phi_mock, err_mock = measure_mock_total_mf(path_to_mocks)
+
+    print("Measuring colour mass function")
+    smf_red, smf_blue  = measure_mock_colour_mf(path_to_mocks)
 
     print("Measuring covariance matrix")
     measure_cov_mat(phi_mock)
@@ -501,9 +633,12 @@ def main(args):
     print("Measuring correlation matrix")
     measure_corr_mat(phi_mock)
 
-    print("Plotting mass functions for mock and data")
-    plot_all_mf(max_mock, phi_mock, err_mock, max_data, phi_data, err_data, 
+    print("Plotting total mass functions for mocks and data")
+    plot_total_mf(max_mock, phi_mock, err_mock, max_data, phi_data, err_data, 
         counts_data)
+    
+    print("Plotting colour mass functions for mocks")
+    plot_colour_mf(smf_red, smf_blue)
     
     print("Comparing cosmic and poisson error for {0}".format(survey))
     cosmic_vs_poisson(catl_file)
