b9d6dbfee5fddeefd834d34f6538859443efbe0e
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2020-05-12 21:43:31 -0500

    deleted catalogs added group finder script added custom legend

--- src/mcmc/modified_colour_smf_from_chain.py
+++ src/mcmc/modified_colour_smf_from_chain.py
@@ -534,7 +534,7 @@ def populate_mock(theta, model):
             limit = np.round(np.log10((10**9.1) / 2.041), 1)
     sample_mask = model_init.mock.galaxy_table['stellar_mass'] >= 10**limit
     gals = model.mock.galaxy_table[sample_mask]
-    gals_df = gals.to_pandas()
+    gals_df = pd.DataFrame(np.array(gals))
 
     return gals_df
 
@@ -1103,7 +1103,7 @@ def plot_mf(result, red_data, blue_data, maxis_bf_red, phi_bf_red,
                 color=orig_handle[1])
             return [l1, l2] 
 
-    fig1= plt.figure(figsize=(10,10))
+    
 
     maxis_red_data, phi_red_data, err_red_data = red_data[0], red_data[1], \
         red_data[2]
@@ -1243,21 +1243,122 @@ total_data[2], red_data[2], blue_data[2] = \
 model_init = halocat_init(halo_catalog, z_median)
 
 print('Populating halos using behroozi shmr params')
+# Random subset of grey lines from 68th percentile param values smhm_run6
 param_subset = pd.read_csv('data/processed/test.csv', sep=',')
 param_subset = param_subset.drop(columns=['Unnamed: 0'])
 param_subset = param_subset.loc[:,'mhalo_c':'scatter']
-gals_df_ = populate_mock(bf_params_shmr, model_init)
-gals_df_ = assign_cen_sat_flag(gals_df_)
-# gals_df_ = gals_df_[['stellar_mass', 'C_S', 'halo_mvir', 'halo_mvir_host_halo',\
-#     'halo_macc','halo_hostid', 'halo_id']]
 
-print('Multiprocessing')
-result = mp_init(mcmc_table_pctl, nproc)
+fig1= plt.figure(figsize=(10,10))
+for idx in range(len(param_subset)):
+    # Slicing row by row of param values
+    param_set = param_subset.iloc[idx:idx+1,:].values[0]
+    gals_df_ = populate_mock(param_set, model_init)
+    gals_df_ = assign_cen_sat_flag(gals_df_)
+    gals_df_ = gals_df_[['stellar_mass', 'C_S', 'halo_mvir', \
+        'halo_mvir_host_halo', 'halo_macc','halo_hostid', 'halo_id']]
+    print('Getting best fit model')
+    maxis_bf_red, phi_bf_red, maxis_bf_blue, phi_bf_blue, cen_gals_red, \
+        cen_halos_red, cen_gals_blue, cen_halos_blue = \
+        get_best_fit_model(bf_params)
 
-print('Getting best fit model')
-maxis_bf_red, phi_bf_red, maxis_bf_blue, phi_bf_blue, cen_gals_red, \
-    cen_halos_red, cen_gals_blue, cen_halos_blue = get_best_fit_model(bf_params)
 
-plot_mf(result, red_data, blue_data, maxis_bf_red, phi_bf_red, 
-    maxis_bf_blue, phi_bf_blue, bf_chi2)
+    print('Multiprocessing')
+    result = mp_init(mcmc_table_pctl, nproc)
+    
+    class AnyObjectHandler(HandlerBase): 
+        def create_artists(self, legend, orig_handle, x0, y0, width, height, 
+            fontsize, trans):
+            if orig_handle[3]:
+                topcap_r = plt.Line2D([x0,x0+width*0.2], [0.8*height, 0.8*height], 
+                    linestyle='-', color='darkred') 
+                body_r = plt.Line2D([x0+width*0.1, x0+width*0.1], \
+                    [0.2*height, 0.8*height], linestyle='-', color='darkred')
+                bottomcap_r = plt.Line2D([x0, x0+width*0.2], \
+                    [0.2*height, 0.2*height], linestyle='-', color='darkred')
+                topcap_b = plt.Line2D([x0+width*0.4, x0+width*0.6], \
+                    [0.8*height, 0.8*height], linestyle='-', color='darkblue') 
+                body_b = plt.Line2D([x0+width*0.5, x0+width*0.5], \
+                    [0.2*height, 0.8*height], linestyle='-', color='darkblue')
+                bottomcap_b = plt.Line2D([x0+width*0.4, x0+width*0.6], \
+                    [0.2*height, 0.2*height], linestyle='-', color='darkblue')
+                return [topcap_r, body_r, bottomcap_r, topcap_b, body_b, bottomcap_b]
+            l1 = plt.Line2D([x0, x0+width], [0.7*height, 0.7*height], 
+                linestyle=orig_handle[2], color=orig_handle[0]) 
+            l2 = plt.Line2D([x0, x0+width], [0.3*height, 0.3*height],  
+                linestyle=orig_handle[2],
+                color=orig_handle[1])
+            return [l1, l2] 
+
+    maxis_red_data, phi_red_data, err_red_data = red_data[0], red_data[1], \
+        red_data[2]
+    maxis_blue_data, phi_blue_data, err_blue_data = blue_data[0], blue_data[1], \
+        blue_data[2]
+    lower_err = phi_red_data - err_red_data
+    upper_err = phi_red_data + err_red_data
+    lower_err = phi_red_data - lower_err
+    upper_err = upper_err - phi_red_data
+    asymmetric_err = [lower_err, upper_err]
+    plt.errorbar(maxis_red_data,phi_red_data,yerr=asymmetric_err,
+        color='darkred',fmt='s', ecolor='darkred',markersize=5,capsize=5,
+        capthick=0.5,label='data',zorder=10)
+    lower_err = phi_blue_data - err_blue_data
+    upper_err = phi_blue_data + err_blue_data
+    lower_err = phi_blue_data - lower_err
+    upper_err = upper_err - phi_blue_data
+    asymmetric_err = [lower_err, upper_err]
+    plt.errorbar(maxis_blue_data,phi_blue_data,yerr=asymmetric_err,
+        color='darkblue',fmt='s', ecolor='darkblue',markersize=5,capsize=5,
+        capthick=0.5,label='data',zorder=10)
+    for idx in range(len(result[0][0])):
+        plt.plot(result[0][0][idx],result[0][1][idx],color='indianred',
+            linestyle='-',alpha=0.3,zorder=0,label='model')
+    for idx in range(len(result[0][2])):
+        plt.plot(result[0][2][idx],result[0][3][idx],color='cornflowerblue',
+            linestyle='-',alpha=0.3,zorder=0,label='model')
+    for idx in range(len(result[1][0])):
+        plt.plot(result[1][0][idx],result[1][1][idx],color='indianred',
+            linestyle='-',alpha=0.3,zorder=0)
+    for idx in range(len(result[1][2])):
+        plt.plot(result[1][2][idx],result[1][3][idx],color='cornflowerblue',
+            linestyle='-',alpha=0.3,zorder=0)
+    for idx in range(len(result[2][0])):
+        plt.plot(result[2][0][idx],result[2][1][idx],color='indianred',
+            linestyle='-',alpha=0.3,zorder=0)
+    for idx in range(len(result[2][2])):
+        plt.plot(result[2][2][idx],result[2][3][idx],color='cornflowerblue',
+            linestyle='-',alpha=0.3,zorder=0)
+    for idx in range(len(result[3][0])):
+        plt.plot(result[3][0][idx],result[3][1][idx],color='indianred',
+            linestyle='-',alpha=0.3,zorder=0)
+    for idx in range(len(result[3][2])):
+        plt.plot(result[3][2][idx],result[3][3][idx],color='cornflowerblue',
+            linestyle='-',alpha=0.3,zorder=0)
+    for idx in range(len(result[4][0])):
+        plt.plot(result[4][0][idx],result[4][1][idx],color='indianred',
+            linestyle='-',alpha=0.3,zorder=0)
+    for idx in range(len(result[4][2])):
+        plt.plot(result[4][2][idx],result[4][3][idx],color='cornflowerblue',
+            linestyle='-',alpha=0.3,zorder=0)
+    # REMOVED BEST FIT ERROR
+    plt.errorbar(maxis_bf_red,phi_bf_red,
+        color='darkred',fmt='-s',ecolor='darkred',markersize=3,lw=3,
+        capsize=5,capthick=0.5,label='best fit',zorder=10)
+    plt.errorbar(maxis_bf_blue,phi_bf_blue,
+        color='darkblue',fmt='-s',ecolor='darkblue',markersize=3,lw=3,
+        capsize=5,capthick=0.5,label='best fit',zorder=10)
+    plt.ylim(-5,-1)
+    if mf_type == 'smf':
+        plt.xlabel(r'\boldmath$\log_{10}\ M_\star \left[\mathrm{M_\odot}\, \mathrm{h}^{-1} \right]$', fontsize=20)
+    elif mf_type == 'bmf':
+        plt.xlabel(r'\boldmath$\log_{10}\ M_{b} \left[\mathrm{M_\odot}\, \mathrm{h}^{-1} \right]$', fontsize=20)
+    plt.ylabel(r'\boldmath$\Phi \left[\mathrm{dex}^{-1}\,\mathrm{Mpc}^{-3}\,\mathrm{h}^{3} \right]$', fontsize=20)
+    plt.legend([("darkred", "darkblue", "-", False), \
+        ("indianred","cornflowerblue", "-", False), (0, 0, False, True)],\
+        ["best fit", "models", "data"], handler_map={tuple: AnyObjectHandler()},\
+        loc='best', prop={'size': 20})
+    plt.annotate(r'$\boldsymbol\chi ^2 \approx$ {0}'.format(np.round(bf_chi2,2)), 
+        xy=(0.1, 0.1), xycoords='axes fraction', bbox=dict(boxstyle="square", 
+        ec='k', fc='lightgray', alpha=0.5), size=15)
+plt.show()
+
 
---
+++ src/mocks_analysis/group_finder.py
@@ -0,0 +1,450 @@
+"""
+{This TEST script applies redshift-space distortions to Vishnu mock and runs 
+ group finder so that velocity dispersion measurements can be obtained since 
+ we are including that as a second observable in constraining red and blue 
+ SHMRs.}
+"""
+
+from halotools.empirical_models import PrebuiltSubhaloModelFactory
+from halotools.sim_manager import CachedHaloCatalog
+from cosmo_utils.utils import work_paths as cwpaths
+from astropy.cosmology import LambdaCDM
+from scipy.interpolate import interp1d
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import subprocess
+import os
+
+os.chdir('/fs1/caldervf/Repositories/Large_Scale_Structure/ECO/ECO_Mocks_Catls')
+import src.data.utilities_python as cu
+os.chdir('/fs1/masad/Research/Repositories/RESOLVE_Statistics') 
+
+__author__ = '{Mehnaaz Asad}'
+
+def read_data_catl(path_to_file, survey):
+    """
+    Reads survey catalog from file
+
+    Parameters
+    ----------
+    path_to_file: `string`
+        Path to survey catalog file
+
+    survey: `string`
+        Name of survey
+
+    Returns
+    ---------
+    catl: `pandas.DataFrame`
+        Survey catalog with grpcz, abs rmag and stellar mass limits
+    
+    volume: `float`
+        Volume of survey
+
+    cvar: `float`
+        Cosmic variance of survey
+
+    z_median: `float`
+        Median redshift of survey
+    """
+    if survey == 'eco':
+        # 13878 galaxies
+        eco_buff = pd.read_csv(path_to_file,delimiter=",", header=0)
+
+        if mf_type == 'smf':
+            # 6456 galaxies                       
+            catl = eco_buff.loc[(eco_buff.grpcz.values >= 3000) & 
+                (eco_buff.grpcz.values <= 7000) & 
+                (eco_buff.absrmag.values <= -17.33)]
+        elif mf_type == 'bmf':
+            catl = eco_buff.loc[(eco_buff.grpcz.values >= 3000) & 
+                (eco_buff.grpcz.values <= 7000) & 
+                (eco_buff.absrmag.values <= -17.33)] 
+
+        volume = 151829.26 # Survey volume without buffer [Mpc/h]^3
+        cvar = 0.125
+        z_median = np.median(catl.grpcz.values) / (3 * 10**5)
+        
+    elif survey == 'resolvea' or survey == 'resolveb':
+        # 2286 galaxies
+        resolve_live18 = pd.read_csv(path_to_file, delimiter=",", header=0)
+
+        if survey == 'resolvea':
+            if mf_type == 'smf':
+                catl = resolve_live18.loc[
+                    (resolve_live18.grpcz.values >= 4500) & 
+                    (resolve_live18.grpcz.values <= 7000) & 
+                    (resolve_live18.absrmag.values <= -17.33) & 
+                    (resolve_live18.logmstar.values >= 8.9)]
+            elif mf_type == 'bmf':
+                catl = resolve_live18.loc[(resolve_live18.f_a.values == 1) & 
+                    (resolve_live18.grpcz.values >= 4500) & 
+                    (resolve_live18.grpcz.values <= 7000) & 
+                    (resolve_live18.absrmag.values <= -17.33)]
+
+            volume = 13172.384  # Survey volume without buffer [Mpc/h]^3
+            cvar = 0.30
+            z_median = np.median(resolve_live18.grpcz.values) / (3 * 10**5)
+        
+        elif survey == 'resolveb':
+            if mf_type == 'smf':
+                # 487 - cz, 369 - grpcz
+                catl = resolve_live18.loc[(resolve_live18.f_b.values == 1) & 
+                    (resolve_live18.grpcz.values >= 4500) & 
+                    (resolve_live18.grpcz.values <= 7000) & 
+                    (resolve_live18.absrmag.values <= -17) & 
+                    (resolve_live18.logmstar.values >= 8.7)]
+            elif mf_type == 'bmf':
+                catl = resolve_live18.loc[(resolve_live18.f_b.values == 1) & 
+                    (resolve_live18.grpcz.values >= 4500) & 
+                    (resolve_live18.grpcz.values <= 7000) & 
+                    (resolve_live18.absrmag.values <= -17)]
+
+            volume = 4709.8373  # *2.915 #Survey volume without buffer [Mpc/h]^3
+            cvar = 0.58
+            z_median = np.median(resolve_live18.grpcz.values) / (3 * 10**5)
+
+    return catl,volume,cvar,z_median
+
+def halocat_init(halo_catalog, z_median):
+    """
+    Initial population of halo catalog using populate_mock function
+
+    Parameters
+    ----------
+    halo_catalog: string
+        Path to halo catalog
+    
+    z_median: float
+        Median redshift of survey
+
+    Returns
+    ---------
+    model: halotools model instance
+        Model based on behroozi 2010 SMHM
+    """
+    halocat = CachedHaloCatalog(fname=halo_catalog, update_cached_fname=True)
+    model = PrebuiltSubhaloModelFactory('behroozi10', redshift=z_median, \
+        prim_haloprop_key='halo_macc')
+    model.populate_mock(halocat,seed=5)
+
+    return model
+
+def populate_mock(theta, model):
+    """
+    Populate mock based on five SMHM parameter values and model
+
+    Parameters
+    ----------
+    theta: array
+        Array of parameter values
+    
+    model: halotools model instance
+        Model based on behroozi 2010 SMHM
+
+    Returns
+    ---------
+    gals_df: pandas dataframe
+        Dataframe of mock catalog
+    """
+    """"""
+
+    mhalo_characteristic, mstellar_characteristic, mlow_slope, mhigh_slope,\
+        mstellar_scatter = theta
+    model.param_dict['smhm_m1_0'] = mhalo_characteristic
+    model.param_dict['smhm_m0_0'] = mstellar_characteristic
+    model.param_dict['smhm_beta_0'] = mlow_slope
+    model.param_dict['smhm_delta_0'] = mhigh_slope
+    model.param_dict['scatter_model_param1'] = mstellar_scatter
+
+    model.mock.populate()
+
+    if survey == 'eco' or survey == 'resolvea':
+        if mf_type == 'smf':
+            limit = np.round(np.log10((10**8.9) / 2.041), 1)
+        elif mf_type == 'bmf':
+            limit = np.round(np.log10((10**9.4) / 2.041), 1)
+    elif survey == 'resolveb':
+        if mf_type == 'smf':
+            limit = np.round(np.log10((10**8.7) / 2.041), 1)
+        elif mf_type == 'bmf':
+            limit = np.round(np.log10((10**9.1) / 2.041), 1)
+    sample_mask = model_init.mock.galaxy_table['stellar_mass'] >= 10**limit
+    gals = model.mock.galaxy_table[sample_mask]
+    gals_df = pd.DataFrame(np.array(gals))
+
+    return gals_df
+
+def cart_to_spherical_coords(cart_arr, dist):
+    """
+    Computes the right ascension and declination for the given 
+    point in (x,y,z) position
+    
+    Parameters
+    -----------
+    cart_arr: numpy.ndarray, shape (3,)
+        array with (x,y,z) positions
+    dist: float
+        dist to the point from observer's position
+        
+    Returns
+    -----------
+    ra_val: float
+        right ascension of the point on the sky
+    dec_val: float
+        declination of the point on the sky
+    """
+    
+    ## Reformatting coordinates
+    # Cartesian coordinates
+    (   x_val,
+        y_val,
+        z_val) = cart_arr/float(dist)
+    # Distance to object
+    dist = float(dist)
+    ## Declination
+    dec_val = 90. - np.degrees(np.arccos(z_val))
+    ## Right ascension
+    if x_val == 0:
+        if y_val > 0.:
+            ra_val = 90.
+        elif y_val < 0.:
+            ra_val = -90.
+    else:
+        ra_val = np.degrees(np.arctan(y_val/x_val))
+
+    ## Seeing on which quadrant the point is at
+    if x_val < 0.:
+        ra_val += 180.
+    elif (x_val >= 0.) and (y_val < 0.):
+        ra_val += 360.
+
+    return ra_val, dec_val
+
+def apply_rsd(mock_catalog):
+    """
+    Applies redshift-space distortions
+
+    Parameters
+    ----------
+    mock_catalog: Pandas dataframe
+        Galaxy catalog
+    
+    Returns
+    ---------
+    mock_catalog: Pandas dataframe
+        Mock catalog with redshift-space distortions now applied and
+        ra,dec,rsd positions and velocity information added
+    """
+
+    ngal = len(mock_catalog)
+    speed_c = 3*10**5 #km/s
+    z_min = 0
+    z_max = 0.5
+    dz = 10**-3
+    H0 = 100
+    omega_m = 0.25
+    omega_b = 0.04
+    Tcmb0 = 2.7255
+    
+    redshift_arr = np.arange(z_min,z_max,dz)
+    cosmo = LambdaCDM(H0,omega_m,omega_b,Tcmb0)
+    como_dist = cosmo.comoving_distance(redshift_arr)
+    comodist_z_interp = interp1d(como_dist,redshift_arr)
+    
+    cart_gals = mock_catalog[['x','y','z']].values #Mpc/h
+    vel_gals = mock_catalog[['vx','vy','vz']].values #km/s
+    
+    dist_from_obs_arr = np.zeros(ngal)
+    ra_arr = np.zeros(ngal)
+    dec_arr = np.zeros(ngal)
+    cz_arr = np.zeros(ngal)
+    cz_nodist_arr = np.zeros(ngal)
+    vel_tan_arr = np.zeros(ngal)
+    vel_tot_arr = np.zeros(ngal)
+    vel_pec_arr = np.zeros(ngal)
+    for x in tqdm(range(ngal)):
+        dist_from_obs = (np.sum(cart_gals[x]**2))**.5
+        z_cosm = comodist_z_interp(dist_from_obs)
+        cz_cosm = speed_c * z_cosm
+        cz_val = cz_cosm
+        ra,dec = cart_to_spherical_coords(cart_gals[x],dist_from_obs)
+        vr = np.dot(cart_gals[x], vel_gals[x])/dist_from_obs
+        #this cz includes hubble flow and peculiar motion
+        cz_val += vr*(1+z_cosm) 
+        vel_tot = (np.sum(vel_gals[x]**2))**.5
+        vel_tan = (vel_tot**2 - vr**2)**.5
+        vel_pec  = (cz_val - cz_cosm)/(1 + z_cosm)
+        dist_from_obs_arr[x] = dist_from_obs
+        ra_arr[x] = ra
+        dec_arr[x] = dec
+        cz_arr[x] = cz_val
+        cz_nodist_arr[x] = cz_cosm
+        vel_tot_arr[x] = vel_tot
+        vel_tan_arr[x] = vel_tan
+        vel_pec_arr[x] = vel_pec
+    
+    mock_catalog['r_dist'] = dist_from_obs_arr
+    mock_catalog['ra'] = ra_arr
+    mock_catalog['dec'] = dec_arr
+    mock_catalog['cz'] = cz_arr
+    mock_catalog['cz_nodist'] = cz_nodist_arr
+    mock_catalog['vel_tot'] = vel_tot_arr
+    mock_catalog['vel_tan'] = vel_tan_arr
+    mock_catalog['vel_pec'] = vel_pec_arr
+
+    return mock_catalog
+
+def group_finding(mock_pd, mock_zz_file, param_dict, file_ext='csv'):
+    """
+    Runs the group finder `FoF` on the file, and assigns galaxies to 
+    galaxy groups
+    Parameters
+    -----------
+    mock_pd: pandas DataFrame
+        DataFrame with positions, velocities, and more for the 
+        galaxies that made it into the catalogue
+    mock_zz_file: string
+        path to the galaxy catalogue
+    param_dict: python dictionary
+        dictionary with `project` variables
+    proj_dict: python dictionary
+        Dictionary with current and new paths to project directories
+    file_ext: string, optional (default = 'csv')
+        file extension for the FoF file products
+    Returns
+    -----------
+    mockgal_pd_merged: pandas DataFrame
+        DataFrame with the info on each mock galaxy + their group properties
+    mockgroup_pd: pandas DataFrame
+        DataFrame with the info on each mock galaxy group
+    """
+    ## Constants
+    Prog_msg = param_dict['Prog_msg']
+    if param_dict['verbose']:
+        print('{0} Group Finding ....'.format(Prog_msg))
+    # Speed of light - in km/s
+    speed_c = param_dict['c']
+    ##
+    ## Running FoF
+    # File prefix
+
+    # Defining files for FoF output and Mock coordinates
+    fof_file        = '{0}.galcatl_fof.{1}'.format(mock_zz_file, file_ext)
+    grep_file       = '{0}.galcatl_grep.{1}'.format(mock_zz_file, file_ext)
+    grep_g_file     = '{0}.galcatl_grep_g.{1}'.format(mock_zz_file, file_ext)
+    mock_coord_path = '{0}.galcatl_radeccz.{1}'.format(mock_zz_file, file_ext)
+    ## RA-DEC-CZ file
+    mock_coord_pd = mock_pd[['ra','dec','cz']].to_csv(mock_coord_path,
+                        sep=' ', header=None, index=False)
+    cu.File_Exists(mock_coord_path)
+    ## Creating `FoF` command and executing it
+    fof_exe = os.path.join( cu.get_code_c(), 'bin', 'fof9_ascii')
+    cu.File_Exists(fof_exe)
+    # FoF command
+    fof_str = '{0} {1} {2} {3} {4} {5} {6} {7} > {8}'
+    fof_arr = [ fof_exe,
+                param_dict['survey_vol'],
+                param_dict['zmin'],
+                param_dict['zmax'],
+                param_dict['l_perp'],
+                param_dict['l_para'],
+                param_dict['nmin'],
+                mock_coord_path,
+                fof_file]
+    fof_cmd = fof_str.format(*fof_arr)
+    # Executing command
+    if param_dict['verbose']:
+        print(fof_cmd)
+    subprocess.call(fof_cmd, shell=True)
+    ##
+    ## Parsing `fof_file` - Galaxy and Group files
+    gal_cmd   = 'grep G -v {0} > {1}'.format(fof_file, grep_file)
+    group_cmd = 'grep G    {0} > {1}'.format(fof_file, grep_g_file)
+    # Running commands
+    if param_dict['verbose']:
+        print(gal_cmd  )
+        print(group_cmd)
+    subprocess.call(gal_cmd  , shell=True)
+    subprocess.call(group_cmd, shell=True)
+    ##
+    ## Extracting galaxy and group information
+    # Column names
+    gal_names   = ['groupid', 'galid', 'ra', 'dec', 'z']
+    group_names = [ 'G', 'groupid', 'cen_ra', 'cen_dec', 'cen_z', 'ngals',\
+                    'sigma_v', 'rproj']
+    # Pandas DataFrames
+    # Galaxies
+    grep_pd = pd.read_csv(grep_file, sep='\s+', header=None, names=gal_names,
+        index_col='galid').sort_index()
+    grep_pd.index.name = None
+    # Converting redshift to velocity
+    grep_pd.loc[:,'cz'] = grep_pd['z'] * speed_c
+    grep_pd = grep_pd.drop('z', axis=1)
+    # Galaxy groups
+    mockgroup_pd = pd.read_csv(grep_g_file, sep='\s+', header=None, 
+        names=group_names)
+    # Group centroid velocity
+    mockgroup_pd.loc[:,'cen_cz'] = mockgroup_pd['cen_z'] * speed_c
+    mockgroup_pd = mockgroup_pd.drop('cen_z', axis=1)
+    mockgroup_pd = mockgroup_pd.drop('G', axis=1)
+    ## Joining the 2 datasets for galaxies
+    mockgal_pd_merged = pd.concat([mock_pd, grep_pd['groupid']], axis=1)
+    # Removing `1` from `groupid`
+    mockgroup_pd.loc     [:,'groupid'] -= 1
+    mockgal_pd_merged.loc[:,'groupid'] -= 1
+    ## Removing FoF files
+    if param_dict['verbose']:
+        print('{0} Removing group-finding related files'.format(
+            param_dict['Prog_msg']))
+    os.remove(fof_file)
+    os.remove(grep_file)
+    os.remove(grep_g_file)
+    os.remove(mock_coord_path)
+    Prog_msg = param_dict['Prog_msg']
+    if param_dict['verbose']:
+        print('{0} Group Finding ....Done'.format(Prog_msg))
+
+    return mockgal_pd_merged, mockgroup_pd
+
+global survey
+global mf_type
+global param_dict
+survey = 'eco'
+machine = 'mac'
+mf_type = 'smf'
+
+eco = {
+    'c': 3*10**5, 
+    'survey_vol': 151829.26, (Mpc/h)^3
+    'min_cz' : 3000,
+    'max_cz' : 7000,
+    'mag_limit' : -17.33,
+    'mstar_limit' : 8.9,
+    'zmin': 3000/3*10**5, # without buffer
+    'zmax': 7000/3*10**5,    
+    'l_perp': 0.07,
+    'l_para': 1.1,
+    'nmin': 1
+}
+
+# Changes string name of survey to variable so that the survey dict can 
+# be accessed
+param_dict = vars()[survey]
+
+dict_of_paths = cwpaths.cookiecutter_paths()
+path_to_raw = dict_of_paths['raw_dir']
+path_to_data = dict_of_paths['data_dir']
+
+halo_catalog = '/home/asadm2/.astropy/cache/halotools/halo_catalogs/'\
+    'vishnu/rockstar/vishnu_rockstar_test.hdf5'
+
+if survey == 'eco':
+    catl_file = path_to_raw + "eco/eco_all.csv"
+
+catl, volume, cvar, z_median = read_data_catl(catl_file, survey)
+model_init = halocat_init(halo_catalog, z_median)
+params = np.array([12.32381675, 10.56581819, 0.4276319, 0.7457711, 0.34784431])
+gals_df = populate_mock(params, model_init)
+gals_df_rsd = apply_rsd(gals_df)
