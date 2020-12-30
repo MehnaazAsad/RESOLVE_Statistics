7f594e0656fb4c555e078c2199fa4b46451ba3fc
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2020-12-29 18:19:51 -0600

    Removed temporary code after main()

--- src/mcmc/mcmc_colour.py
+++ src/mcmc/mcmc_colour.py
@@ -1672,8 +1672,3 @@ if __name__ == '__main__':
 
     # with open('profile_eco_2p_16w_5s.txt', 'w+') as f:
     #     f.write(s.getvalue())
-    
-
-
-
-nproc, nwalkers, nsteps, phi_red_data, phi_blue_data, std_red_data, std_blue_data, err, corr_mat_inv = nproc, nwalkers, nsteps, red_data[1], blue_data[1], std_red, std_blue, sigma, corr_mat_inv
