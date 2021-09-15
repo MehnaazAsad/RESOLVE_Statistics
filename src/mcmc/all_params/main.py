39faee621bbfe2ac09e83cfacf05b5fa31b2d155
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2021-09-14 21:09:09 -0500

    changing implementation from staticmethods to instances and member functions

--- src/mcmc/all_params/main.py
+++ src/mcmc/all_params/main.py
@@ -6,11 +6,25 @@ __author__ = '{Mehnaaz Asad}'
 from settings import Settings
 from preprocess import Preprocess
 from analysis import Analysis
+from experiments import Experiments
+from plotting import Plotting
 
-def main():
-    Settings.Initalize_Global_Settings()
-    Preprocess.Load_Into_Dataframes()
-    Analysis.Core()
+def main():    
+    settings = Settings()
+    settings.Initalize_Global_Settings()
+
+    preprocess = Preprocess(settings)
+    preprocess.Load_Into_Dataframes()
+    
+    analysis = Analysis(preprocess)
+    experiments = Experiments(analysis)
+
+    analysis.Core(experiments)
+    experiments.Run_Experiments()
+    
+    models = analysis.Mocks_And_Models(experiments)
+    # Plotting.Plot_Core()
+    # Plotting.Plot_Experiments()
 
 
 if __name__ == '__main__':
