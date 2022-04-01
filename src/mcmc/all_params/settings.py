e48810dae5fe8a82133d69c8fd841a3bbe5cdaed
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2022-03-31 21:35:19 -0500

    Updated data catalog used and run numbers for latest hybrid and halo chains

--- src/mcmc/all_params/settings.py
+++ src/mcmc/all_params/settings.py
@@ -9,7 +9,7 @@ from cosmo_utils.utils import work_paths as cwpaths
 class Settings():
 
     def __init__(self) -> None:
-        self.quenching = 'halo'
+        self.quenching = 'hybrid'
         self.machine = 'mac'
         self.survey = 'eco'
         self.mf_type = 'smf'
@@ -37,9 +37,9 @@ class Settings():
             self.halo_catalog = path_to_raw + 'vishnu_rockstar_test.hdf5'
 
         if self.quenching == 'halo':
-            self.run = 41
+            self.run = 44
         elif self.quenching == 'hybrid':
-            self.run = 42
+            self.run = 43
 
         if self.run >= 37:
             self.chi2_file = self.path_to_proc + \
@@ -61,7 +61,7 @@ class Settings():
             ## Updated catalog with group finder run on subset after applying M* 
             # and cz cuts: changed volume to be in h=1 instead of 0.7
             self.catl_file = self.path_to_proc + \
-                "gal_group_eco_data_buffer_volh1.hdf5"
+                "gal_group_eco_data_buffer_volh1_dr2.hdf5"
             self.path_to_mocks = path_to_data + 'mocks/m200b/eco/'
         elif self.survey == 'resolvea' or self.survey == 'resolveb':
             self.catl_file = path_to_raw + "RESOLVE_liveJune2018.csv"
